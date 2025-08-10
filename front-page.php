4aa11436582d346cd3cc98071d6b43f9157d0702
Ken Vilar <kenvilar@gmail.com>
2025-08-10 22:28:05 +0800

    Done homepage latest podcast episodes section

--- front-page.php
+++ front-page.php
@@ -1,12 +1,18 @@
 <?php
+
 /* Homepage */
 get_header();
 ?>
 	<div class="page-wrapper">
 		<div class="main-wrapper">
-			<?php get_template_part('components/hero/home'); ?>
-			<?php get_template_part('components/section/home/featured-content'); ?>
-			<?php get_template_part('components/section/home/connect-your-brand-with-an-active-global-audience'); ?>
+			<?php
+			get_template_part('components/hero/home'); ?>
+			<?php
+			get_template_part('components/section/home/featured-content'); ?>
+			<?php
+			get_template_part('components/section/home/connect-your-brand-with-an-active-global-audience'); ?>
+			<?php
+			get_template_part('components/section/home/latest-podcast-episodes'); ?>
 		</div>
 	</div>
 <?php
