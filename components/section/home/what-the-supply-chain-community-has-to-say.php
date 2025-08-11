7e04540209b2d8df9cc8e5805dfbb3bc2170d8a3
Ken Vilar <kenvilar@gmail.com>
2025-08-12 00:49:04 +0800

    Separate the splide arrows

--- components/section/home/what-the-supply-chain-community-has-to-say.php
+++ components/section/home/what-the-supply-chain-community-has-to-say.php
@@ -68,34 +68,9 @@
 								endif; ?>
 							</div>
 						</div>
-						<div class="splide__arrows">
-							<div class="splide__arrow splide__arrow--prev">
-								<div class="flex w-embed">
-									<svg style="background: white;border-radius: 50%;" xmlns="http://www.w3.org/2000/svg" width="42"
-									     height="42" viewBox="0 0 42 42" fill="none">
-										<path
-											d="M21 41C9.95431 41 1 32.0457 1 21C1 9.95431 9.95431 1 21 1C32.0457 1 41 9.95431 41 21C41 32.0457 32.0457 41 21 41Z"
-											stroke="#FFAB56" stroke-width="2" stroke-miterlimit="10"></path>
-										<path d="M28.5912 21.4893H13.0004" stroke="#FFAB56" stroke-width="2" stroke-miterlimit="10"></path>
-										<path d="M19.4894 15L13 21.4894L19.4894 27.9787" stroke="#FFAB56" stroke-width="2"
-										      stroke-miterlimit="10"></path>
-									</svg>
-								</div>
-							</div>
-							<div class="splide__arrow splide__arrow--next">
-								<div class="flex w-embed">
-									<svg style="background: white;border-radius: 50%;" xmlns="http://www.w3.org/2000/svg" width="42"
-									     height="42" viewBox="0 0 42 42" fill="none">
-										<path
-											d="M21 41C32.0457 41 41 32.0457 41 21C41 9.95431 32.0457 1 21 1C9.95431 1 1 9.95431 1 21C1 32.0457 9.95431 41 21 41Z"
-											stroke="#FFAB56" stroke-width="2" stroke-miterlimit="10"></path>
-										<path d="M13 21.4893H28.5908" stroke="#FFAB56" stroke-width="2" stroke-miterlimit="10"></path>
-										<path d="M22.1017 15L28.5911 21.4894L22.1017 27.9787" stroke="#FFAB56" stroke-width="2"
-										      stroke-miterlimit="10"></path>
-									</svg>
-								</div>
-							</div>
-						</div>
+						<?php
+						get_template_part('components/splide-arrows');
+						?>
 						<div class="display-none w-embed">
 							<style>
 								[slider-4] .splide__list {
