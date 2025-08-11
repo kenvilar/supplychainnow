5a9adbca8bcf9425ce336ab92905aa4b6d57ddc0
Ken Vilar <kenvilar@gmail.com>
2025-08-11 16:27:34 +0800

    Dynamic loop for awards and recognitions slider

--- components/section/our-story/awards-and-recognitions.php
+++ components/section/our-story/awards-and-recognitions.php
@@ -17,201 +17,24 @@
 		</div>
 	</div>
 	<div slider-1="" class="splide">
-		<div class="splide__track">
+		<div class="splide__track overflow-visible!">
 			<div class="splide__list">
-				<div class="splide__slide splide__slide--clone" id="splide01-clone01" role="group"
-				     aria-roledescription="slide" aria-label="2 of 8" style="margin-right: 37.91px; width: 200px;"
-				     aria-hidden="true">
-					<div class="awards-pentagon-block">
-						<img
-							src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/6892f6d7022c0f45bbc73ea5_about--awards-7.avif"
-							loading="lazy" alt="" class="image h-auto">
+				<?php
+				// Number of images you have
+				$total_awards = 8;
+
+				for ($i = 1; $i <= $total_awards; $i++) : ?>
+					<div class="splide__slide">
+						<div class="awards-pentagon-block">
+							<img
+								src="<?php
+								echo get_stylesheet_directory_uri() . "/assets/img/our-story/about--awards-{$i}.avif"; ?>"
+								loading="lazy" alt="Award <?php
+							echo $i; ?>" class="image h-auto">
+						</div>
 					</div>
-				</div>
-				<div class="splide__slide splide__slide--clone" id="splide01-clone02" role="group"
-				     aria-roledescription="slide" aria-label="3 of 8" style="margin-right: 37.91px; width: 200px;"
-				     aria-hidden="true">
-					<div class="awards-pentagon-block">
-						<img
-							src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/6892f6d7677852fabf8687c2_about--awards-6.avif"
-							loading="lazy" alt="" class="image h-auto">
-					</div>
-				</div>
-				<div class="splide__slide splide__slide--clone is-active" id="splide01-clone03" role="group"
-				     aria-roledescription="slide" aria-label="4 of 8" style="margin-right: 37.91px; width: 200px;"
-				     aria-hidden="true">
-					<div class="awards-pentagon-block">
-						<img
-							src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/6892f6d70893a22835a0c467_about--awards-5.avif"
-							loading="lazy" alt="" class="image h-auto">
-					</div>
-				</div>
-				<div class="splide__slide splide__slide--clone" id="splide01-clone04" role="group"
-				     aria-roledescription="slide" aria-label="5 of 8" style="margin-right: 37.91px; width: 200px;"
-				     aria-hidden="true">
-					<div class="awards-pentagon-block">
-						<img
-							src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/6892f6d76f19c8856bf1bc0b_about--awards-4.avif"
-							loading="lazy" alt="" class="image h-auto">
-					</div>
-				</div>
-				<div class="splide__slide splide__slide--clone" id="splide01-clone05" role="group"
-				     aria-roledescription="slide" aria-label="6 of 8" style="margin-right: 37.91px; width: 200px;"
-				     aria-hidden="true">
-					<div class="awards-pentagon-block">
-						<img
-							src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/6892f6d717eda80d8d850246_about--awards-3.avif"
-							loading="lazy" alt="" class="image h-auto">
-					</div>
-				</div>
-				<div class="splide__slide splide__slide--clone" id="splide01-clone06" role="group"
-				     aria-roledescription="slide" aria-label="7 of 8" style="margin-right: 37.91px; width: 200px;"
-				     aria-hidden="true">
-					<div class="awards-pentagon-block">
-						<img
-							src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/6892f6d7f5dc60318fe38718_about--awards-2.avif"
-							loading="lazy" alt="" class="image h-auto">
-					</div>
-				</div>
-				<div class="splide__slide splide__slide--clone" id="splide01-clone07" role="group"
-				     aria-roledescription="slide" aria-label="8 of 8" style="margin-right: 37.91px; width: 200px;"
-				     aria-hidden="true">
-					<div class="awards-pentagon-block">
-						<img
-							src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/6892f6d72b8c421e711780c5_about--awards-1.avif"
-							loading="lazy" alt="" class="image h-auto">
-					</div>
-				</div>
-				<div class="splide__slide" id="splide01-slide01" role="group" aria-roledescription="slide"
-				     aria-label="1 of 8"
-				     style="margin-right: 37.91px; width: 200px;" aria-hidden="true">
-					<div class="awards-pentagon-block">
-						<img
-							src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/6892f6d72ec99b24ba4d1c09_about--awards-8.avif"
-							loading="lazy" alt="" class="image h-auto">
-					</div>
-				</div>
-				<div class="splide__slide" id="splide01-slide02" role="group" aria-roledescription="slide"
-				     aria-label="2 of 8"
-				     style="margin-right: 37.91px; width: 200px;" aria-hidden="true">
-					<div class="awards-pentagon-block">
-						<img
-							src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/6892f6d7022c0f45bbc73ea5_about--awards-7.avif"
-							loading="lazy" alt="" class="image h-auto">
-					</div>
-				</div>
-				<div class="splide__slide is-prev" id="splide01-slide03" role="group" aria-roledescription="slide"
-				     aria-label="3 of 8" style="margin-right: 37.91px; width: 200px;" aria-hidden="true">
-					<div class="awards-pentagon-block">
-						<img
-							src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/6892f6d7677852fabf8687c2_about--awards-6.avif"
-							loading="lazy" alt="" class="image h-auto">
-					</div>
-				</div>
-				<div class="splide__slide is-visible is-active" id="splide01-slide04" role="group"
-				     aria-roledescription="slide" aria-label="4 of 8" style="margin-right: 37.91px; width: 200px;">
-					<div class="awards-pentagon-block">
-						<img
-							src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/6892f6d70893a22835a0c467_about--awards-5.avif"
-							loading="lazy" alt="" class="image h-auto">
-					</div>
-				</div>
-				<div class="splide__slide is-visible is-next" id="splide01-slide05" role="group"
-				     aria-roledescription="slide"
-				     aria-label="5 of 8" style="margin-right: 37.91px; width: 200px;">
-					<div class="awards-pentagon-block">
-						<img
-							src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/6892f6d76f19c8856bf1bc0b_about--awards-4.avif"
-							loading="lazy" alt="" class="image h-auto">
-					</div>
-				</div>
-				<div class="splide__slide is-visible" id="splide01-slide06" role="group" aria-roledescription="slide"
-				     aria-label="6 of 8" style="margin-right: 37.91px; width: 200px;">
-					<div class="awards-pentagon-block">
-						<img
-							src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/6892f6d717eda80d8d850246_about--awards-3.avif"
-							loading="lazy" alt="" class="image h-auto">
-					</div>
-				</div>
-				<div class="splide__slide is-visible" id="splide01-slide07" role="group" aria-roledescription="slide"
-				     aria-label="7 of 8" style="margin-right: 37.91px; width: 200px;">
-					<div class="awards-pentagon-block">
-						<img
-							src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/6892f6d7f5dc60318fe38718_about--awards-2.avif"
-							loading="lazy" alt="" class="image h-auto">
-					</div>
-				</div>
-				<div class="splide__slide is-visible" id="splide01-slide08" role="group" aria-roledescription="slide"
-				     aria-label="8 of 8" style="margin-right: 37.91px; width: 200px;">
-					<div class="awards-pentagon-block">
-						<img
-							src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/6892f6d72b8c421e711780c5_about--awards-1.avif"
-							loading="lazy" alt="" class="image h-auto">
-					</div>
-				</div>
-				<div class="splide__slide splide__slide--clone" id="splide01-clone08" role="group"
-				     aria-roledescription="slide" aria-label="1 of 8" style="margin-right: 37.91px; width: 200px;"
-				     aria-hidden="true">
-					<div class="awards-pentagon-block">
-						<img
-							src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/6892f6d72ec99b24ba4d1c09_about--awards-8.avif"
-							loading="lazy" alt="" class="image h-auto">
-					</div>
-				</div>
-				<div class="splide__slide splide__slide--clone" id="splide01-clone09" role="group"
-				     aria-roledescription="slide" aria-label="2 of 8" style="margin-right: 37.91px; width: 200px;"
-				     aria-hidden="true">
-					<div class="awards-pentagon-block">
-						<img
-							src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/6892f6d7022c0f45bbc73ea5_about--awards-7.avif"
-							loading="lazy" alt="" class="image h-auto">
-					</div>
-				</div>
-				<div class="splide__slide splide__slide--clone" id="splide01-clone10" role="group"
-				     aria-roledescription="slide" aria-label="3 of 8" style="margin-right: 37.91px; width: 200px;"
-				     aria-hidden="true">
-					<div class="awards-pentagon-block">
-						<img
-							src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/6892f6d7677852fabf8687c2_about--awards-6.avif"
-							loading="lazy" alt="" class="image h-auto">
-					</div>
-				</div>
-				<div class="splide__slide splide__slide--clone is-active" id="splide01-clone11" role="group"
-				     aria-roledescription="slide" aria-label="4 of 8" style="margin-right: 37.91px; width: 200px;"
-				     aria-hidden="true">
-					<div class="awards-pentagon-block">
-						<img
-							src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/6892f6d70893a22835a0c467_about--awards-5.avif"
-							loading="lazy" alt="" class="image h-auto">
-					</div>
-				</div>
-				<div class="splide__slide splide__slide--clone" id="splide01-clone12" role="group"
-				     aria-roledescription="slide" aria-label="5 of 8" style="margin-right: 37.91px; width: 200px;"
-				     aria-hidden="true">
-					<div class="awards-pentagon-block">
-						<img
-							src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/6892f6d76f19c8856bf1bc0b_about--awards-4.avif"
-							loading="lazy" alt="" class="image h-auto">
-					</div>
-				</div>
-				<div class="splide__slide splide__slide--clone" id="splide01-clone13" role="group"
-				     aria-roledescription="slide" aria-label="6 of 8" style="margin-right: 37.91px; width: 200px;"
-				     aria-hidden="true">
-					<div class="awards-pentagon-block">
-						<img
-							src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/6892f6d717eda80d8d850246_about--awards-3.avif"
-							loading="lazy" alt="" class="image h-auto">
-					</div>
-				</div>
-				<div class="splide__slide splide__slide--clone" id="splide01-clone14" role="group"
-				     aria-roledescription="slide" aria-label="7 of 8" style="margin-right: 37.91px; width: 200px;"
-				     aria-hidden="true">
-					<div class="awards-pentagon-block">
-						<img
-							src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/6892f6d7f5dc60318fe38718_about--awards-2.avif"
-							loading="lazy" alt="" class="image h-auto">
-					</div>
-				</div>
+				<?php
+				endfor; ?>
 			</div>
 		</div>
 		<div class="display-none w-embed">
