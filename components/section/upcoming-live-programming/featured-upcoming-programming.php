5c7565af8a5ff867b53563e4111623e0cb7dd669
Ken Vilar <kenvilar@gmail.com>
2025-08-12 00:42:28 +0800

    Add WP_Query in featured upcoming events

--- components/section/upcoming-live-programming/featured-upcoming-programming.php
+++ components/section/upcoming-live-programming/featured-upcoming-programming.php
@@ -16,163 +16,229 @@
 				<div class="w-layout-blockcontainer max-w-1252 w-container">
 					<div class="w-dyn-list">
 						<div role="list" class="flex justify-center gap-28 sm-flex-col w-dyn-items">
-							<div role="listitem" class="_w-full group w-dyn-item">
-								<a href="#" class="flex flex-col gap-20 h-full w-inline-block">
-									<div class="_w-full h-full">
-										<div class="mb-28">
-											<div class="overflow-hidden rounded-12 relative h-344 bg--cargogrey">
-												<img
-													src="https://cdn.prod.website-files.com/686225582293e0967d2b9375/68937ce3e7ccfa3fb32d1ac4_image8.jpeg"
-													loading="lazy" alt="Remote work best practices webinar" class="image relative opacity-40">
-												<div class="absolute absolute--tl p-24">
-													<div style="background-color:#ffab56" class="rounded-full text--white py-4 px-8">
-														<div class="font-semibold text-2xs uppercase">User Experience</div>
+							<?php
+							$q = new WP_Query([
+								'post_type' => 'page',
+								'post_status' => 'publish',
+								'posts_per_page' => 2,
+								'offset' => 0,
+								'meta_query' => [
+									'relation' => 'AND',
+									[
+										'relation' => 'OR',
+										[
+											'key' => '_wp_page_template',
+											'value' => 'episode-detail.php',
+											'compare' => '=',
+										],
+										[
+											'key' => '_wp_page_template',
+											'value' => 'webinar-detail.php',
+											'compare' => '=',
+										],
+										[
+											'key' => '_wp_page_template',
+											'value' => 'livestream-detail.php',
+											'compare' => '=',
+										],
+									],
+									[
+										'key' => 'select_media_type',
+										'value' => ['podcast', 'livestream', 'webinar'],
+										'compare' => 'IN',
+										'type' => 'CHAR',
+									],
+								],
+								'date_query' => [
+									[
+										'after' => current_time('Y-m-d'),
+										'inclusive' => true, // include today
+									]
+								],
+								'orderby' => ['menu_order' => 'ASC', 'date' => 'DESC'],
+							]);
+
+							if ($q->have_posts()): ?>
+								<?php
+								while ($q->have_posts()): $q->the_post(); ?>
+									<a href="<?php
+									the_permalink(); ?>" class="relative w-full group">
+										<div class="relative flex flex-col justify-between gap-20 h-full">
+											<div class="w-full">
+												<div class="mb-28">
+													<div class="overflow-hidden rounded-12 relative h-344 bg-cargogrey">
+														<img
+															src="<?php
+															echo get_the_post_thumbnail()
+																? the_post_thumbnail_url()
+																: get_stylesheet_directory_uri(
+																) . '/assets/img/misc/default-card-img-thumbnail.avif' ?>"
+															loading="lazy" alt="" class="image relative opacity-40">
+														<?php
+														$terms = get_the_terms(get_the_ID(), 'tags');
+														if (!is_wp_error($terms) && !empty($terms)) {
+															$first = array_values($terms)[0];
+															?>
+															<div class="absolute absolute--tl p-24 flex items-center justify-center">
+																<div class="relative rounded-full overflow-hidden py-4 px-8">
+																	<div class="relative font-semibold uppercase text-2xs text-white lh-full z-10">
+																		<?php
+																		echo $first->name; ?>
+																	</div>
+																	<?php
+																	echo get_field(
+																		'select_media_type'
+																	) == 'livestream'
+																		? '<div class="absolute absolute--full bg-primary"></div>'
+																		: '';
+																	?>
+																	<?php
+																	echo get_field(
+																		'select_media_type'
+																	) == 'podcast'
+																		? '<div class="absolute absolute--full bg-secondary"></div>'
+																		: '';
+																	?>
+																	<?php
+																	echo get_field(
+																		'select_media_type'
+																	) == 'webinar'
+																		? '<div class="absolute absolute--full bg-tertiary"></div>'
+																		: '';
+																	?>
+																</div>
+															</div>
+															<?php
+														}
+														?>
+														<div
+															class="absolute absolute--full flex items-center justify-center translate-y-220 group-hover:translate-y-0 transition-all duration-500">
+															<?php
+															if (get_field('select_media_type') == 'livestream') {
+																?>
+																<img
+																	src="<?php
+																	echo get_stylesheet_directory_uri(
+																		) . '/assets/img/icons/play-button-livestream.avif'; ?>"
+																	loading="lazy" alt="play-button-livestream">
+																<?php
+															}
+															?>
+															<?php
+															if (get_field('select_media_type') == 'podcast') {
+																?>
+																<img
+																	src="<?php
+																	echo get_stylesheet_directory_uri(
+																		) . '/assets/img/icons/play-button-podcast.avif'; ?>"
+																	loading="lazy" alt="play-button-podcast">
+																<?php
+															}
+															?>
+															<?php
+															if (get_field('select_media_type') == 'webinar') {
+																?>
+																<img
+																	src="<?php
+																	echo get_stylesheet_directory_uri(
+																		) . '/assets/img/icons/play-button-webinar.avif'; ?>"
+																	loading="lazy" alt="play-button-webinar">
+																<?php
+															}
+															?>
+														</div>
 													</div>
 												</div>
-												<div
-													class="absolute absolute--full flex items-center justify-center translate-y-220 group-hover-translate-y-0 transition-all duration-500">
-													<img
-														src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/688a36f89ae4a406c3dbc9a5_play-bg--primary.avif"
-														loading="lazy" alt="play-bg--primary">
-													<img
-														src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/688a3474d97d0c9a0c4acafd_play-bg--tertiary.avif"
-														loading="lazy" alt="play-bg--tertiary" class="w-condition-invisible">
+												<div class="mb-12">
+													<div class="flex items-center gap-32 sm:flex-wrap sm:gap-8">
+														<div class="flex items-center gap-8">
+															<div class="flex items-center">
+																<?php
+																if (get_field('select_media_type') == 'livestream') {
+																	?>
+																	<img
+																		src="<?php
+																		echo get_stylesheet_directory_uri(
+																			) . '/assets/img/icons/livestream-card-icon.svg'; ?>"
+																		loading="lazy" alt="livestream-music">
+																	<?php
+																}
+																?>
+																<?php
+																if (get_field('select_media_type') == 'podcast') {
+																	?>
+																	<img
+																		class="size-24"
+																		src="<?php
+																		echo get_stylesheet_directory_uri() . '/assets/img/icons/podcast-card-icon.png'; ?>"
+																		loading="lazy" alt="podcast-blue-microphone">
+																	<?php
+																}
+																?>
+																<?php
+																if (get_field('select_media_type') == 'webinar') {
+																	?>
+																	<img
+																		class="size-24"
+																		src="<?php
+																		echo get_stylesheet_directory_uri() . '/assets/img/icons/webinar-card-icon.png'; ?>"
+																		loading="lazy" alt="webinar-person">
+																	<?php
+																}
+																?>
+															</div>
+															<?php
+															if (get_field('select_media_type')) {
+																?>
+																<div class="font-family-secondary text-sm capitalize">
+																	<?php
+																	echo the_field('select_media_type'); ?>
+																</div>
+																<?php
+															}
+															?>
+														</div>
+														<div class="flex items-center gap-8 text-sm font-light font-family-secondary">
+															<div><?php
+																the_date(); ?></div>
+															<!--<div>•</div>
+															<div>6 min 25 sec</div>-->
+														</div>
+													</div>
 												</div>
+												<h3 class="font-semibold"><?php
+													the_title(); ?></h3>
+											</div>
+											<div class="w-full tracking-[1.6px]" scn-text-limit="2">
+												<?php
+												if (get_the_excerpt()) {
+													the_excerpt();
+												} else {
+													if (get_field('livestream_description')) {
+														the_field('livestream_description');
+													} else {
+														if (get_field('episode_summary')) {
+															the_field('episode_summary');
+														} else {
+															if (get_field('webinar_description')) {
+																the_field('webinar_description');
+															} else {
+																echo '';
+															}
+														}
+													}
+												}
+												?>
 											</div>
 										</div>
-										<div class="mb-12">
-											<div class="flex items-center gap-32 sm-flex-wrap sm-gap-8">
-												<div class="flex items-center gap-8">
-													<div class="flex w-condition-invisible w-embed">
-														<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-														     fill="none">
-															<circle cx="12" cy="12" r="12" fill="#4E88B6"></circle>
-															<path
-																d="M8.9 12.025H9.14V16.32H8.9C8.61766 16.32 8.33808 16.2644 8.07723 16.1563C7.81638 16.0483 7.57937 15.8899 7.37972 15.6903C7.18007 15.4906 7.02171 15.2536 6.91366 14.9928C6.80561 14.7319 6.75 14.4523 6.75 14.17C6.75132 13.6006 6.97843 13.0551 7.38149 12.6529C7.78455 12.2508 8.33065 12.025 8.9 12.025Z"
-																fill="#4E88B6" stroke="white" stroke-width="1.5" stroke-miterlimit="10"></path>
-															<path
-																d="M14.8651 12.025H15.1051C15.6753 12.025 16.2222 12.2515 16.6254 12.6547C17.0286 13.0579 17.2551 13.6048 17.2551 14.175C17.2551 14.7452 17.0286 15.2921 16.6254 15.6953C16.2222 16.0985 15.6753 16.325 15.1051 16.325H14.8651V12.025Z"
-																fill="#4E88B6" stroke="white" stroke-width="1.5" stroke-miterlimit="10"></path>
-															<path
-																d="M7.70508 12.48V11.05C7.70508 9.91044 8.15743 8.81747 8.96275 8.01121C9.76808 7.20494 10.8605 6.75133 12.0001 6.75C13.1396 6.75133 14.2321 7.20494 15.0374 8.01121C15.8427 8.81747 16.2951 9.91044 16.2951 11.05V12.48"
-																fill="#4E88B6"></path>
-															<path
-																d="M7.70508 12.48V11.05C7.70508 9.91044 8.15743 8.81747 8.96275 8.01121C9.76808 7.20494 10.8605 6.75133 12.0001 6.75C13.1396 6.75133 14.2321 7.20494 15.0374 8.01121C15.8427 8.81747 16.2951 9.91044 16.2951 11.05V12.48"
-																stroke="white" stroke-width="1.5" stroke-miterlimit="10"></path>
-														</svg>
-													</div>
-													<div class="flex w-embed">
-														<svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" viewBox="0 0 19 19"
-														     fill="none">
-															<g clip-path="url(#clip0_2036_37)">
-																<circle cx="9.5" cy="9.5" r="9.5" fill="#FFAB56"></circle>
-																<circle cx="9.5" cy="9.5" r="4.5" fill="white"></circle>
-															</g>
-															<defs>
-																<clipPath id="clip0_2036_37">
-																	<rect width="19" height="19" fill="white"></rect>
-																</clipPath>
-															</defs>
-														</svg>
-													</div>
-													<img
-														src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/688a2f9dc4c41532e44fa96e_webinar-person.avif"
-														loading="lazy" alt="webinar-person" class="w-condition-invisible">
-													<div class="font-family-secondary text-xs">Livestream</div>
-												</div>
-												<div class="flex items-center gap-8">
-													<div class="font-family-secondary font-light text-xs">October 20, 2023</div>
-													<div>•</div>
-													<div class="font-family-secondary font-light text-xs">1.5 hours</div>
-												</div>
-											</div>
-										</div>
-										<h3 class="font-semibold">Webinar on Remote Work Best Practices</h3>
-									</div>
-									<div class="_w-full">
-										<p>Join our webinar to discover best practices for remote work.</p>
-									</div>
-								</a>
-							</div>
-							<div role="listitem" class="_w-full group w-dyn-item">
-								<a href="#" class="flex flex-col gap-20 h-full w-inline-block">
-									<div class="_w-full h-full">
-										<div class="mb-28">
-											<div class="overflow-hidden rounded-12 relative h-344 bg--cargogrey">
-												<img
-													src="https://cdn.prod.website-files.com/686225582293e0967d2b9375/68937a163918d6a1775ef71b_image16.jpeg"
-													loading="lazy" alt="Mental health awareness livestream" class="image relative opacity-40">
-												<div class="absolute absolute--tl p-24">
-													<div style="background-color:hsla(0, 0.00%, 100.00%, 1.00)"
-													     class="rounded-full text--white py-4 px-8">
-														<div class="font-semibold text-2xs uppercase">Search Engine Optimization</div>
-													</div>
-												</div>
-												<div
-													class="absolute absolute--full flex items-center justify-center translate-y-220 group-hover-translate-y-0 transition-all duration-500">
-													<img
-														src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/688a36f89ae4a406c3dbc9a5_play-bg--primary.avif"
-														loading="lazy" alt="play-bg--primary" class="w-condition-invisible">
-													<img
-														src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/688a3474d97d0c9a0c4acafd_play-bg--tertiary.avif"
-														loading="lazy" alt="play-bg--tertiary" class="w-condition-invisible">
-												</div>
-											</div>
-										</div>
-										<div class="mb-12">
-											<div class="flex items-center gap-32 sm-flex-wrap sm-gap-8">
-												<div class="flex items-center gap-8">
-													<div class="flex w-condition-invisible w-embed">
-														<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-														     fill="none">
-															<circle cx="12" cy="12" r="12" fill="#4E88B6"></circle>
-															<path
-																d="M8.9 12.025H9.14V16.32H8.9C8.61766 16.32 8.33808 16.2644 8.07723 16.1563C7.81638 16.0483 7.57937 15.8899 7.37972 15.6903C7.18007 15.4906 7.02171 15.2536 6.91366 14.9928C6.80561 14.7319 6.75 14.4523 6.75 14.17C6.75132 13.6006 6.97843 13.0551 7.38149 12.6529C7.78455 12.2508 8.33065 12.025 8.9 12.025Z"
-																fill="#4E88B6" stroke="white" stroke-width="1.5" stroke-miterlimit="10"></path>
-															<path
-																d="M14.8651 12.025H15.1051C15.6753 12.025 16.2222 12.2515 16.6254 12.6547C17.0286 13.0579 17.2551 13.6048 17.2551 14.175C17.2551 14.7452 17.0286 15.2921 16.6254 15.6953C16.2222 16.0985 15.6753 16.325 15.1051 16.325H14.8651V12.025Z"
-																fill="#4E88B6" stroke="white" stroke-width="1.5" stroke-miterlimit="10"></path>
-															<path
-																d="M7.70508 12.48V11.05C7.70508 9.91044 8.15743 8.81747 8.96275 8.01121C9.76808 7.20494 10.8605 6.75133 12.0001 6.75C13.1396 6.75133 14.2321 7.20494 15.0374 8.01121C15.8427 8.81747 16.2951 9.91044 16.2951 11.05V12.48"
-																fill="#4E88B6"></path>
-															<path
-																d="M7.70508 12.48V11.05C7.70508 9.91044 8.15743 8.81747 8.96275 8.01121C9.76808 7.20494 10.8605 6.75133 12.0001 6.75C13.1396 6.75133 14.2321 7.20494 15.0374 8.01121C15.8427 8.81747 16.2951 9.91044 16.2951 11.05V12.48"
-																stroke="white" stroke-width="1.5" stroke-miterlimit="10"></path>
-														</svg>
-													</div>
-													<div class="flex w-condition-invisible w-embed">
-														<svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" viewBox="0 0 19 19"
-														     fill="none">
-															<g clip-path="url(#clip0_2036_37)">
-																<circle cx="9.5" cy="9.5" r="9.5" fill="#FFAB56"></circle>
-																<circle cx="9.5" cy="9.5" r="4.5" fill="white"></circle>
-															</g>
-															<defs>
-																<clipPath id="clip0_2036_37">
-																	<rect width="19" height="19" fill="white"></rect>
-																</clipPath>
-															</defs>
-														</svg>
-													</div>
-													<img
-														src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/688a2f9dc4c41532e44fa96e_webinar-person.avif"
-														loading="lazy" alt="webinar-person" class="w-condition-invisible">
-													<div class="font-family-secondary text-xs">Article</div>
-												</div>
-												<div class="flex items-center gap-8">
-													<div class="font-family-secondary font-light text-xs">October 10, 2023</div>
-													<div>•</div>
-													<div class="font-family-secondary font-light text-xs">2 hours</div>
-												</div>
-											</div>
-										</div>
-										<h3 class="font-semibold">Livestream on Mental Health Awareness</h3>
-									</div>
-									<div class="_w-full">
-										<p>Join our livestream to discuss mental health awareness and resources.</p>
-									</div>
-								</a>
-							</div>
+									</a>
+								<?php
+								endwhile;
+								wp_reset_postdata(); ?>
+							<?php
+							else:
+								echo 'No upcoming events found.';
+							endif; ?>
 						</div>
 					</div>
 				</div>
