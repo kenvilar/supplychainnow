28f657e16ca23d4cab3436094c1d0c0a8dc6c812
Ken Vilar <kenvilar@gmail.com>
2025-08-12 12:50:48 +0800

    Done episode detail page

--- components/line-with-blinking-dot.php
+++ components/line-with-blinking-dot.php
@@ -1,7 +1,8 @@
 <?php
 
+$maxWidthClassnames = $args['maxWidthClassnames'] ?? '';
 ?>
-<div class="max-w-136 w-full h-1 relative bg-cargogrey/25 w-container">
+<div class="max-w-136 w-full h-1 relative bg-cargogrey/25 mx-auto <?= esc_attr($maxWidthClassnames); ?>">
 	<div class="absolute absolute--r flex items-center pr-32">
 		<div blinking-dot class="size-8 rounded-8 bg--primary"></div>
 	</div>
