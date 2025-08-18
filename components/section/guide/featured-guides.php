3a37bb015b12993be0ef6185f98a53c650761adf
Ken Vilar <kenvilar@gmail.com>
2025-08-18 21:03:35 +0800

    Separate the sections of Guides page

---
+++ components/section/guide/featured-guides.php
@@ -0,0 +1,110 @@
+<?php
+
+?>
+<section class="section">
+	<div class="site-padding sm:py-60 py-40">
+		<div class="w-layout-blockcontainer max-w-1248 w-container">
+			<div class="mb-44">
+				<div class="mb-20">
+					<h2 class="text-center">Featured Guides</h2>
+				</div>
+				<div class="w-layout-blockcontainer max-w-136 w-full h-1 relative bg-cargogrey/25 w-container">
+					<div class="absolute absolute--r flex items-center pr-32">
+						<div blinking-dot="" class="size-8 rounded-8 bg-primary"></div>
+					</div>
+				</div>
+			</div>
+			<div class="w-full w-dyn-list">
+				<div role="list" class="flex justify-center gap-28 sm:flex-col w-dyn-items">
+					<div role="listitem" class="w-full group w-dyn-item">
+						<div class="flex flex-col gap-20 h-full">
+							<div class="w-full h-full">
+								<div class="mb-28">
+									<div class="overflow-hidden rounded-12 relative h-344 bg-cargogrey">
+										<img
+											src="https://cdn.prod.website-files.com/686225582293e0967d2b9375/68918f24e08a123821c1ad85_image19.jpeg"
+											loading="lazy" alt="A futuristic representation of CMS technology."
+											class="image relative opacity-40">
+										<div class="w-dyn-list">
+											<div role="list" class="w-dyn-items">
+												<div role="listitem" class="w-dyn-item">
+													<div class="absolute absolute--tl p-24">
+														<div class="rounded-full py-4 px-8 bg-white">
+															<div class="font-semibold text-2xs uppercase lh-full">User Experience</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="mb-12">
+									<div class="flex items-center gap-32 sm:flex-wrap sm:gap-8">
+										<div class="flex items-center gap-8">
+											<div class="font-family-secondary text-xs">Guide</div>
+										</div>
+										<div class="flex items-center gap-8">
+											<div class="font-family-secondary font-light text-xs">October 9, 2023</div>
+											<div>•</div>
+											<div class="font-family-secondary font-light text-xs">6 minutes</div>
+										</div>
+									</div>
+								</div>
+								<h3 class="font-semibold">Future Trends in Content Management Systems</h3>
+							</div>
+							<div class="w-full">
+								<div class="rt--plain truncate-2-lines-fallback w-richtext">Future of CMSThe landscape of content
+									management systems is constantly evolving. Emerging trends such as AI integration, headle...
+								</div>
+							</div>
+						</div>
+					</div>
+					<div role="listitem" class="w-full group w-dyn-item">
+						<div class="flex flex-col gap-20 h-full">
+							<div class="w-full h-full">
+								<div class="mb-28">
+									<div class="overflow-hidden rounded-12 relative h-344 bg-cargogrey">
+										<img
+											src="https://cdn.prod.website-files.com/686225582293e0967d2b9375/68906c785bd2e45f2be0fdba_image14.jpeg"
+											loading="lazy" alt="Graphs and charts representing analytics."
+											class="image relative opacity-40">
+										<div class="w-dyn-list">
+											<div role="list" class="w-dyn-items">
+												<div role="listitem" class="w-dyn-item">
+													<div class="absolute absolute--tl p-24">
+														<div class="rounded-full py-4 px-8 bg-white">
+															<div class="font-semibold text-2xs uppercase lh-full">Web Development</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="mb-12">
+									<div class="flex items-center gap-32 sm:flex-wrap sm:gap-8">
+										<div class="flex items-center gap-8">
+											<div class="font-family-secondary text-xs">Guide</div>
+										</div>
+										<div class="flex items-center gap-8">
+											<div class="font-family-secondary font-light text-xs">October 7, 2023</div>
+											<div>•</div>
+											<div class="font-family-secondary font-light text-xs">7 minutes</div>
+										</div>
+									</div>
+								</div>
+								<h3 class="font-semibold">Using Analytics for CMS Improvement</h3>
+							</div>
+							<div class="w-full">
+								<div class="rt--plain truncate-2-lines-fallback w-richtext">Analytics in CMSAnalytics play a vital
+									role in understanding how users interact with your content. By analyzing data, you can make
+									informe...
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
