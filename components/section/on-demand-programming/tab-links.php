e15ada4c5b903680d8c5a6a7574e393d9fd614d3
Ken Vilar <kenvilar@gmail.com>
2025-08-18 12:41:19 +0800

    Separate the section of On-Demand Programming page

---
+++ components/section/on-demand-programming/tab-links.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+<section class="section">
+	<div class="site-padding py-0">
+		<div class="w-layout-blockcontainer max-w-full w-container">
+			<div class="flex justify-center xs:flex-col">
+				<a href="/on-demand-programming" aria-current="page"
+				   class="on-demand-programming-link w-inline-block w--current">
+					<div>All Content</div>
+				</a>
+				<a href="/podcasts-and-livestreams" class="on-demand-programming-link w-inline-block">
+					<div>Podcasts &amp; Livestreams</div>
+				</a>
+				<a href="/webinars" class="on-demand-programming-link w-inline-block">
+					<div>Webinars</div>
+				</a>
+			</div>
+		</div>
+	</div>
+	<div class="w-full h-1 bg-cargogrey/25 opacity-75 xs:display-none"></div>
+</section>
