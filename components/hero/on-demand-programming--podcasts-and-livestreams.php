c8e33e57092647e030d57bea94ea4a25be8e8fd1
Ken Vilar <kenvilar@gmail.com>
2025-08-18 13:29:12 +0800

    Separate the section of On-Demand Programming - Podcasts and Livestreams page

--- components/hero/on-demand-programming--podcasts-and-livestreams.php
+++ components/hero/on-demand-programming--podcasts-and-livestreams.php
@@ -1,1 +1,22 @@
 <?php
+
+?>
+<section class="section bg-cargogrey text-white rounded-b-100">
+	<div class="site-padding sm:py-60 pt-200 pb-152 relative z-10">
+		<div class="w-layout-blockcontainer pt-40 w-container text-center max-w-960">
+			<div class="mb-20">
+				<img
+					src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/688dc2a18bab3380ddb6006f_movie.svg"
+					loading="lazy" alt="movie">
+			</div>
+			<div class="mb-16">
+				<h1>On-Demand Programming</h1>
+			</div>
+		</div>
+	</div>
+	<div class="absolute absolute--full w-full h-full">
+		<img
+			src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/688ddc83db1c7699d7b1a40a_hero-on-deman%20programmin%20podcasts%20and%20livestreams.avif"
+			loading="lazy" alt="hero-on-deman programmin podcasts and livestreams" class="image opacity-10">
+	</div>
+</section>
