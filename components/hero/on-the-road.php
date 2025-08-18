0bdd4025d71fb690c97fe047e959eeabb505c9ae
Ken Vilar <kenvilar@gmail.com>
2025-08-18 15:55:12 +0800

    Separate the sections of On The Road page

--- components/hero/on-the-road.php
+++ components/hero/on-the-road.php
@@ -1,1 +1,27 @@
 <?php
+
+?>
+<section class="section bg-cargogrey text-white rounded-b-100">
+	<div class="site-padding sm:py-60 pt-200 pb-116 relative z-10">
+		<div class="w-layout-blockcontainer pt-20 w-container text-center max-w-548">
+			<div class="mb-20">
+				<img
+					src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/687614272c917096266ced65_on-the-road.svg"
+					loading="lazy" alt="on-the-road icon">
+			</div>
+			<div class="mb-16">
+				<h1>On The Road</h1>
+			</div>
+			<div>
+				<p>
+					<br>
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="absolute absolute--full w-full h-full">
+		<img
+			src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/687613a5c247b012c355f0d5_hero-on-the-road.avif"
+			loading="lazy" alt="hero-on-the-road" class="image opacity-10">
+	</div>
+</section>
---
+++ components/section/on-the-road/do-you-want-to-partner.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+<div class="overflow-hidden relative rounded-t-100">
+	<section class="section">
+		<div class="site-padding sm:py-60 py-76">
+			<div class="w-layout-blockcontainer max-w-776 w-container">
+				<div class="mb-32">
+					<h2 class="text-center">Do you want to partner with Supply Chain Now at your upcoming event?</h2>
+				</div>
+				<div class="flex justify-center">
+					<a href="/work-with-us" class="btn primary w-inline-block">
+						<div>Partner With Us</div>
+					</a>
+				</div>
+			</div>
+		</div>
+	</section>
+	<div class="absolute absolute--full gradient2 opacity-25 z--1"></div>
+</div>
