42e43cd05027e541a48ea90699281a25a4892c3a
Ken Vilar <kenvilar@gmail.com>
2025-08-18 20:34:42 +0800

    Separate the sections of Blog page

---
+++ components/section/blog/featured-blogs.php
@@ -0,0 +1,109 @@
+<?php
+
+?>
+<section class="section">
+	<div class="site-padding sm:py-60 py-40">
+		<div class="w-layout-blockcontainer max-w-1248 w-container">
+			<div class="mb-44">
+				<div class="mb-20">
+					<h2 class="text-center">Featured Blogs</h2>
+				</div>
+				<div class="w-layout-blockcontainer max-w-136 w-full h-1 relative bg-cargogrey/25 w-container">
+					<div class="absolute absolute--r flex items-center pr-32">
+						<div blinking-dot="" class="size-8 rounded-8 bg-primary"></div>
+					</div>
+				</div>
+			</div>
+			<div class="w-full w-dyn-list">
+				<div role="list" class="flex justify-center gap-28 sm:flex-col w-dyn-items">
+					<div role="listitem" class="w-full group w-dyn-item">
+						<div class="flex flex-col gap-20 h-full">
+							<div class="w-full h-full">
+								<div class="mb-28">
+									<div class="overflow-hidden rounded-12 relative h-344 bg-cargogrey">
+										<img
+											src="https://cdn.prod.website-files.com/688deda2e7d12a851701c2e9/688df013f4bf0358568c946e_image5.jpeg"
+											loading="lazy" alt="Content Strategy" class="image relative opacity-40">
+										<div class="w-dyn-list">
+											<div role="list" class="w-dyn-items">
+												<div role="listitem" class="w-dyn-item">
+													<div class="absolute absolute--tl p-24">
+														<div class="rounded-full py-4 px-8 bg-white">
+															<div class="font-semibold text-2xs uppercase lh-full">Content Management</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="mb-12">
+									<div class="flex items-center gap-32 sm:flex-wrap sm:gap-8">
+										<div class="flex items-center gap-8">
+											<div class="font-family-secondary text-xs">Blog</div>
+										</div>
+										<div class="flex items-center gap-8">
+											<div class="font-family-secondary font-light text-xs">October 9, 2023</div>
+											<div>•</div>
+											<div class="font-family-secondary font-light text-xs">8 minutes</div>
+										</div>
+									</div>
+								</div>
+								<h3 class="font-semibold">Developing a Content Strategy for Your CMS</h3>
+							</div>
+							<div class="w-full">
+								<div class="rt--plain truncate-2-lines-fallback w-richtext">What is a Content Strategy?A content
+									strategy outlines how you will create, publish, and manage content. It is essential for achieving
+									yo...
+								</div>
+							</div>
+						</div>
+					</div>
+					<div role="listitem" class="w-full group w-dyn-item">
+						<div class="flex flex-col gap-20 h-full">
+							<div class="w-full h-full">
+								<div class="mb-28">
+									<div class="overflow-hidden rounded-12 relative h-344 bg-cargogrey">
+										<img
+											src="https://cdn.prod.website-files.com/688deda2e7d12a851701c2e9/688df013f4bf0358568c945c_image3.jpeg"
+											loading="lazy" alt="CMS and SEO" class="image relative opacity-40">
+										<div class="w-dyn-list">
+											<div role="list" class="w-dyn-items">
+												<div role="listitem" class="w-dyn-item">
+													<div class="absolute absolute--tl p-24">
+														<div class="rounded-full py-4 px-8 bg-white">
+															<div class="font-semibold text-2xs uppercase lh-full">User Experience</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="mb-12">
+									<div class="flex items-center gap-32 sm:flex-wrap sm:gap-8">
+										<div class="flex items-center gap-8">
+											<div class="font-family-secondary text-xs">Blog</div>
+										</div>
+										<div class="flex items-center gap-8">
+											<div class="font-family-secondary font-light text-xs">October 7, 2023</div>
+											<div>•</div>
+											<div class="font-family-secondary font-light text-xs">6 minutes</div>
+										</div>
+									</div>
+								</div>
+								<h3 class="font-semibold">How CMS Affects SEO</h3>
+							</div>
+							<div class="w-full">
+								<div class="rt--plain truncate-2-lines-fallback w-richtext">SEO FundamentalsYour CMS can
+									significantly impact your website's SEO. Choosing the right platform is essential for optimizing
+									your co...
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
