f19f7e5092faf2c4fc91fc3948af90d1f65cc3e8
Ken Vilar <kenvilar@gmail.com>
2025-08-18 16:47:26 +0800

    Separate the sections of Campaign Directory page

---
+++ components/hero/campaign-directory.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+<section class="section bg-cargogrey text--white rounded-b-100">
+	<div class="site-padding sm:py-60 pt-200 pb-152 relative z-10">
+		<div class="w-layout-blockcontainer pt-40 w-container text-center max-w-960">
+			<div class="mb-20">
+				<img
+					src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/686b5ed7b668ce59a9f33f23_notebook-with-one-person.svg"
+					loading="lazy" alt="notebook-with-one-person">
+			</div>
+			<div class="mb-16">
+				<h1>Campaign Directory</h1>
+			</div>
+		</div>
+	</div>
+	<div class="absolute absolute--full w-full h-full">
+		<img
+			src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/686b5f26920b2080ff90fbd5_hero-campaign-directory.avif"
+			loading="lazy" alt="hero-campaign-directory" class="image opacity-10">
+	</div>
+</section>
