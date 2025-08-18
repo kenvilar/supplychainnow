2c3711c4754592ec14a618619d1ff56d1102fea8
Ken Vilar <kenvilar@gmail.com>
2025-08-18 16:33:52 +0800

    Separate the sections of Case Studies page

--- components/hero/case-studies-customer-stories.php
+++ components/hero/case-studies-customer-stories.php
@@ -1,1 +1,27 @@
 <?php
+
+?>
+<section class="section bg-cargogrey text-white rounded-b-100">
+	<div class="site-padding sm:py-60 pt-200 pb-116 relative z-10">
+		<div class="w-layout-blockcontainer pt-20 w-container text-center">
+			<div class="mb-20">
+				<img
+					src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/6879e74d071f729dd8db1580_like-icon.svg"
+					loading="lazy" alt="like-icon">
+			</div>
+			<div class="mb-16">
+				<h1>Case Studies &amp; Customer Stories</h1>
+			</div>
+			<div>
+				<p>
+					<br>
+				</p>
+			</div>
+		</div>
+	</div>
+	<div class="absolute absolute--full w-full h-full">
+		<img
+			src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/6879e87475dc2fc9f6c03fbf_hero-case-studies.avif"
+			loading="lazy" alt="hero-case-studies" class="image opacity-10">
+	</div>
+</section>
---
+++ components/section/case-studies-customer-stories/case-study-2.php
@@ -0,0 +1,88 @@
+<?php
+
+?>
+<section class="section">
+	<div class="site-padding sm:py-60 pb-52">
+		<div class="w-layout-blockcontainer max-w-1252 w-container">
+			<div class="flex gap-20 justify-between sm:flex-col">
+				<div class="max-w-608 w-full md:max-w-full">
+					<div class="mb-20">
+						<div class="font-family-alternate font-semibold text-lg text-secondary"><em>Case Study #2</em></div>
+					</div>
+					<div class="mb-36">
+						<div class="max-w-396 w-full md:max-w-full">
+							<h2>F500 Supply Chain Solution Provider</h2>
+						</div>
+					</div>
+					<p class="tracking-[1.6px]">F500 supply chain solution provider specializing in shipping and logistics
+						(“Company B”) partnered with Supply Chain Now on a Campaign
+					</p>
+				</div>
+				<div class="max-w-588 w-full md:max-w-full">
+					<ul role="list" class="accordion w-list-unstyled">
+						<li class="accordion-item">
+							<div class="accordion-item-header">
+								<div class="flex items-center">
+									<div class="font-semibold text-md">+ &nbsp; Brand Awareness / Thought Leadership</div>
+								</div>
+							</div>
+							<div class="accordion-item-body">
+								<div class="accordion-item-body-content">
+									<p class="text-xs tracking-[1.2px]">After growing rapidly (via sales and acquisitions) the last
+										few
+										years, brand clarity and awareness became even more crucial.
+										<br>
+										<br>
+										The Supply Chain Now team helped drive clarity around Company B’s messages and positioned
+										Company B as a Thought Leader in the space.
+									</p>
+								</div>
+							</div>
+							<div class="absolute absolute--full gradient2 -z-1 opacity-25"></div>
+						</li>
+						<li class="accordion-item">
+							<div class="accordion-item-header">
+								<div class="flex items-center">
+									<div class="font-semibold text-md">+ &nbsp; Lead Generation</div>
+								</div>
+							</div>
+							<div class="accordion-item-body">
+								<div class="accordion-item-body-content">
+									<p class="text-xs tracking-[1.2px]">Supply Chain Now Campaigns see anywhere between 500K - 1M
+										impressions across social media.
+										<br>
+										<br>
+										Company B was able to reach new and existing customers by sparking conversation online with
+										the content they created with Supply Chain Now.
+									</p>
+								</div>
+							</div>
+							<div class="absolute absolute--full gradient2 -z-1 opacity-25"></div>
+						</li>
+						<li class="accordion-item">
+							<div class="accordion-item-header">
+								<div class="flex items-center">
+									<div class="font-semibold text-md">+ &nbsp; Content Creation</div>
+								</div>
+							</div>
+							<div class="accordion-item-body">
+								<div class="accordion-item-body-content">
+									<p class="text-xs tracking-[1.2px]">Compared to every other webinar they have done (both
+										internally
+										and with external third parties), the Supply Chain Now webinar beat the others for registrants
+										by 180%!
+										<br>
+										<br>
+										The conversion rate to attendees was 49%, which is more than double that the 20% they usually
+										experience.
+									</p>
+								</div>
+							</div>
+							<div class="absolute absolute--full gradient2 -z-1 opacity-25"></div>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
