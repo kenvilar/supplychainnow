0748a10222046705d505d29c6ec6eb0e291a6077
Ken Vilar <kenvilar@gmail.com>
2025-08-18 16:37:08 +0800

    Separate the sections of Media Kit page

---
+++ components/hero/media-kit.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+<section class="section bg-cargogrey text-white rounded-b-100">
+	<div class="site-padding sm:py-60 pt-200 pb-152 relative z-10">
+		<div class="w-layout-blockcontainer pt-40 w-container text-center max-w-960">
+			<div class="mb-20">
+				<img
+					src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/686a3daa4efddf44684f8b7e_open-envelope.svg"
+					loading="lazy" alt="open envelope">
+			</div>
+			<div class="mb-16">
+				<h1>Media Kit</h1>
+			</div>
+		</div>
+	</div>
+	<div class="absolute absolute--full w-full h-full">
+		<img
+			src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/6876347fb963456b6aea2ade_hero%20media%20kit.avif"
+			loading="lazy" alt="hero media kit" class="image opacity-10">
+	</div>
+</section>
