209ed329037de8a0b67e86a157bd2b1d22be0d1f
Ken Vilar <kenvilar@gmail.com>
2025-08-18 21:00:27 +0800

    Separate the sections of News page

---
+++ components/section/news/featured-news.php
@@ -0,0 +1,115 @@
+<?php
+
+?>
+<section class="section">
+	<div class="site-padding sm:py-60 py-40">
+		<div class="w-layout-blockcontainer max-w-1248 w-container">
+			<div class="mb-44">
+				<div class="mb-20">
+					<h2 class="text-center">Featured News</h2>
+				</div>
+				<div class="w-layout-blockcontainer max-w-136 w-full h-1 relative bg-cargogrey/25 w-container">
+					<div class="absolute absolute--r flex items-center pr-32">
+						<div blinking-dot="" class="size-8 rounded-8 bg-primary"></div>
+					</div>
+				</div>
+			</div>
+			<div class="w-full w-dyn-list">
+				<div role="list" class="flex justify-center gap-28 sm:flex-col w-dyn-items">
+					<div role="listitem" class="w-full group w-dyn-item">
+						<div class="flex flex-col gap-20 h-full">
+							<div class="w-full h-full">
+								<div class="mb-28">
+									<div class="overflow-hidden rounded-12 relative h-344 bg-cargogrey">
+										<img
+											src="https://cdn.prod.website-files.com/686225582293e0967d2b9375/686244618d158d543b297379_image6.jpeg"
+											loading="lazy" alt="Book fair event" class="image relative opacity-40">
+										<div class="w-dyn-list">
+											<div role="list" class="w-dyn-items">
+												<div role="listitem" class="w-dyn-item">
+													<div class="absolute absolute--tl p-24">
+														<div class="rounded-full py-4 px-8 bg-white">
+															<div class="font-semibold text-2xs uppercase lh-full">Digital Marketing</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="mb-12">
+									<div class="flex items-center gap-32 sm:flex-wrap sm:gap-8">
+										<div class="flex items-center gap-8">
+											<div class="font-family-secondary text-xs">News</div>
+										</div>
+										<div class="flex items-center gap-8">
+											<div class="font-family-secondary font-light text-xs">This is some text inside of a div
+												block.
+											</div>
+											<div>•</div>
+											<div class="font-family-secondary font-light text-xs">This is some text inside of a div
+												block.
+											</div>
+										</div>
+									</div>
+								</div>
+								<h3 class="font-semibold">Heading</h3>
+							</div>
+							<div class="w-full">
+								<div class="rt--plain truncate-2-lines-fallback w-richtext">Heading 1Heading 2Heading 3Heading
+									4Heading 5Heading 6Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod te...
+								</div>
+							</div>
+						</div>
+					</div>
+					<div role="listitem" class="w-full group w-dyn-item">
+						<div class="flex flex-col gap-20 h-full">
+							<div class="w-full h-full">
+								<div class="mb-28">
+									<div class="overflow-hidden rounded-12 relative h-344 bg-cargogrey">
+										<img
+											src="https://cdn.prod.website-files.com/686225582293e0967d2b9375/68906ef21802e9cd2440b31b_image19.jpeg"
+											loading="lazy" alt="Earthquake damage in the city" class="image relative opacity-40">
+										<div class="w-dyn-list">
+											<div role="list" class="w-dyn-items">
+												<div role="listitem" class="w-dyn-item">
+													<div class="absolute absolute--tl p-24">
+														<div class="rounded-full py-4 px-8 bg-white">
+															<div class="font-semibold text-2xs uppercase lh-full">Content Management</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="mb-12">
+									<div class="flex items-center gap-32 sm:flex-wrap sm:gap-8">
+										<div class="flex items-center gap-8">
+											<div class="font-family-secondary text-xs">News</div>
+										</div>
+										<div class="flex items-center gap-8">
+											<div class="font-family-secondary font-light text-xs">This is some text inside of a div
+												block.
+											</div>
+											<div>•</div>
+											<div class="font-family-secondary font-light text-xs">This is some text inside of a div
+												block.
+											</div>
+										</div>
+									</div>
+								</div>
+								<h3 class="font-semibold">Heading</h3>
+							</div>
+							<div class="w-full">
+								<div class="rt--plain truncate-2-lines-fallback w-richtext">Heading 1Heading 2Heading 3Heading
+									4Heading 5Heading 6Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod te...
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
