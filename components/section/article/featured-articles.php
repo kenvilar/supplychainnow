4b49c13e83b21f0a348c46ed63d52ec2c8ca20af
Ken Vilar <kenvilar@gmail.com>
2025-08-18 20:57:04 +0800

    Separate the sections of Articles page

---
+++ components/section/article/featured-articles.php
@@ -0,0 +1,107 @@
+<?php
+
+?>
+<section class="section">
+	<div class="site-padding sm:py-60 py-40">
+		<div class="w-layout-blockcontainer max-w-1248 w-container">
+			<div class="mb-44">
+				<div class="mb-20">
+					<h2 class="text-center">Featured Articles</h2>
+				</div>
+				<div class="w-layout-blockcontainer max-w-136 w-full h-1 relative bg-cargogrey/25 w-container">
+					<div class="absolute absolute--r flex items-center pr-32">
+						<div blinking-dot="" class="size-8 rounded-8 bg-primary"></div>
+					</div>
+				</div>
+			</div>
+			<div class="w-full w-dyn-list">
+				<div role="list" class="flex justify-center gap-28 sm:flex-col w-dyn-items">
+					<div role="listitem" class="w-full group w-dyn-item">
+						<div class="flex flex-col gap-20 h-full">
+							<div class="w-full h-full">
+								<div class="mb-28">
+									<div class="overflow-hidden rounded-12 relative h-344 bg-cargogrey">
+										<img
+											src="https://cdn.prod.website-files.com/686225582293e0967d2b9375/68906ef21802e9cd2440b31b_image19.jpeg"
+											loading="lazy" alt="CMS troubleshooting." class="image relative opacity-40">
+										<div class="w-dyn-list">
+											<div role="list" class="w-dyn-items">
+												<div role="listitem" class="w-dyn-item">
+													<div class="absolute absolute--tl p-24">
+														<div class="rounded-full py-4 px-8 bg-white">
+															<div class="font-semibold text-2xs uppercase lh-full">Web Development</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="mb-12">
+									<div class="flex items-center gap-32 sm:flex-wrap sm:gap-8">
+										<div class="flex items-center gap-8">
+											<div class="font-family-secondary text-xs">Article</div>
+										</div>
+										<div class="flex items-center gap-8">
+											<div class="font-family-secondary font-light text-xs">October 9, 2023</div>
+											<div>•</div>
+											<div class="font-family-secondary font-light text-xs">5 minutes</div>
+										</div>
+									</div>
+								</div>
+								<h3 class="font-semibold">CMS Troubleshooting Tips</h3>
+							</div>
+							<div class="w-full">
+								<div class="rt--plain truncate-2-lines-fallback w-richtext">CMS Troubleshooting TipsEncountering
+									issues with your CMS? Here are some troubleshooting tips:1. Check for UpdatesEnsure your CM...
+								</div>
+							</div>
+						</div>
+					</div>
+					<div role="listitem" class="w-full group w-dyn-item">
+						<div class="flex flex-col gap-20 h-full">
+							<div class="w-full h-full">
+								<div class="mb-28">
+									<div class="overflow-hidden rounded-12 relative h-344 bg-cargogrey">
+										<img
+											src="https://cdn.prod.website-files.com/686225582293e0967d2b9375/68906c785bd2e45f2be0fdb4_image7.jpeg"
+											loading="lazy" alt="CMS for e-commerce." class="image relative opacity-40">
+										<div class="w-dyn-list">
+											<div role="list" class="w-dyn-items">
+												<div role="listitem" class="w-dyn-item">
+													<div class="absolute absolute--tl p-24">
+														<div class="rounded-full py-4 px-8 bg-white">
+															<div class="font-semibold text-2xs uppercase lh-full">Digital Marketing</div>
+														</div>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="mb-12">
+									<div class="flex items-center gap-32 sm:flex-wrap sm:gap-8">
+										<div class="flex items-center gap-8">
+											<div class="font-family-secondary text-xs">Article</div>
+										</div>
+										<div class="flex items-center gap-8">
+											<div class="font-family-secondary font-light text-xs">October 7, 2023</div>
+											<div>•</div>
+											<div class="font-family-secondary font-light text-xs">5 minutes</div>
+										</div>
+									</div>
+								</div>
+								<h3 class="font-semibold">Using CMS for E-commerce</h3>
+							</div>
+							<div class="w-full">
+								<div class="rt--plain truncate-2-lines-fallback w-richtext">Using CMS for E-commerceContent
+									management systems can significantly enhance e-commerce operations. Here’s how:1. Produc...
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
