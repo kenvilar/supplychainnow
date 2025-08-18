b913cd65f9c73f156a0494fca076e74bb3019a41
Ken Vilar <kenvilar@gmail.com>
2025-08-18 14:21:42 +0800

    Separate the section of On-Demand Programming - Webinars page

--- components/hero/on-demand-programming--webinars.php
+++ components/hero/on-demand-programming--webinars.php
@@ -1,1 +1,21 @@
 <?php
+
+?>
+<section class="section bg-cargogrey text-white rounded-b-100">
+	<div class="site-padding sm:py-60 pt-200 pb-152 relative z-10">
+		<div class="w-layout-blockcontainer pt-40 w-container text-center max-w-960">
+			<div class="mb-20">
+				<img src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/688dc2a18bab3380ddb6006f_movie.svg"
+				     loading="lazy" alt="movie">
+			</div>
+			<div class="mb-16">
+				<h1>On-Demand Programming</h1>
+			</div>
+		</div>
+	</div>
+	<div class="absolute absolute--full w-full h-full">
+		<img
+			src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/68903fa4fa530f73d9259958_hero--on-deman%20programming--webinars.avif"
+			loading="lazy" alt="hero--on-deman programming--webinars" class="image opacity-10">
+	</div>
+</section>
---
+++ components/section/on-demand-programming--webinars/featured-webinars.php
@@ -0,0 +1,105 @@
+<?php
+
+?>
+<section class="section">
+	<div class="site-padding sm:py-60 py-60 pb-92">
+		<div class="w-layout-blockcontainer max-w-1372 relative w-container">
+			<div class="mb-44">
+				<div class="mb-20">
+					<h2 class="text-center">Featured Webinars</h2>
+				</div>
+				<div class="w-layout-blockcontainer max-w-136 w-full h-1 relative bg-cargogrey/25 w-container">
+					<div class="absolute absolute--r flex items-center pr-32">
+						<div blinking-dot="" class="size-8 rounded-8 bg-primary"></div>
+					</div>
+				</div>
+			</div>
+			<div class="w-layout-blockcontainer max-w-1252 w-container">
+				<div class="w-dyn-list">
+					<div role="list" class="flex justify-center gap-28 sm:flex-col w-dyn-items">
+						<div role="listitem" class="w-full group w-dyn-item">
+							<a href="#" class="flex flex-col gap-20 h-full w-inline-block">
+								<div class="w-full h-full">
+									<div class="mb-28">
+										<div class="overflow-hidden rounded-12 relative h-344 bg-cargogrey">
+											<img
+												src="https://cdn.prod.website-files.com/686225582293e0967d2b9375/68937ce4e7ccfa3fb32d1af8_image15.jpeg"
+												loading="lazy" alt="Future technology podcast episode" class="image relative opacity-40">
+											<div class="absolute absolute--tl p-24">
+												<div style="background-color:#849fad" class="rounded-full text-white py-4 px-8">
+													<div class="font-semibold text-2xs uppercase lh-full">Content Management</div>
+												</div>
+											</div>
+											<div
+												class="absolute absolute--full flex items-center justify-center translate-y-220 group-hover:translate-y-0 transition-all duration-500">
+												<img
+													src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/688a36f89ae4a406c3dbc9a5_play-bg--primary.avif"
+													loading="lazy" alt="play-bg-primary" class="w-condition-invisible">
+												<img
+													src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/688a3474d97d0c9a0c4acafd_play-bg--tertiary.avif"
+													loading="lazy" alt="play-bg-tertiary">
+											</div>
+										</div>
+									</div>
+									<div class="mb-12">
+										<div class="flex items-center gap-32 sm:flex-wrap sm:gap-8">
+											<div class="flex items-center gap-8">
+												<div class="flex w-condition-invisible w-embed">
+													<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+													     fill="none">
+														<circle cx="12" cy="12" r="12" fill="#4E88B6"></circle>
+														<path
+															d="M8.9 12.025H9.14V16.32H8.9C8.61766 16.32 8.33808 16.2644 8.07723 16.1563C7.81638 16.0483 7.57937 15.8899 7.37972 15.6903C7.18007 15.4906 7.02171 15.2536 6.91366 14.9928C6.80561 14.7319 6.75 14.4523 6.75 14.17C6.75132 13.6006 6.97843 13.0551 7.38149 12.6529C7.78455 12.2508 8.33065 12.025 8.9 12.025Z"
+															fill="#4E88B6" stroke="white" stroke-width="1.5" stroke-miterlimit="10"></path>
+														<path
+															d="M14.8651 12.025H15.1051C15.6753 12.025 16.2222 12.2515 16.6254 12.6547C17.0286 13.0579 17.2551 13.6048 17.2551 14.175C17.2551 14.7452 17.0286 15.2921 16.6254 15.6953C16.2222 16.0985 15.6753 16.325 15.1051 16.325H14.8651V12.025Z"
+															fill="#4E88B6" stroke="white" stroke-width="1.5" stroke-miterlimit="10"></path>
+														<path
+															d="M7.70508 12.48V11.05C7.70508 9.91044 8.15743 8.81747 8.96275 8.01121C9.76808 7.20494 10.8605 6.75133 12.0001 6.75C13.1396 6.75133 14.2321 7.20494 15.0374 8.01121C15.8427 8.81747 16.2951 9.91044 16.2951 11.05V12.48"
+															fill="#4E88B6"></path>
+														<path
+															d="M7.70508 12.48V11.05C7.70508 9.91044 8.15743 8.81747 8.96275 8.01121C9.76808 7.20494 10.8605 6.75133 12.0001 6.75C13.1396 6.75133 14.2321 7.20494 15.0374 8.01121C15.8427 8.81747 16.2951 9.91044 16.2951 11.05V12.48"
+															stroke="white" stroke-width="1.5" stroke-miterlimit="10"></path>
+													</svg>
+												</div>
+												<div class="flex w-condition-invisible w-embed">
+													<svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" viewBox="0 0 19 19"
+													     fill="none">
+														<g clip-path="url(#clip0_2036_37)">
+															<circle cx="9.5" cy="9.5" r="9.5" fill="#FFAB56"></circle>
+															<circle cx="9.5" cy="9.5" r="4.5" fill="white"></circle>
+														</g>
+														<defs>
+															<clipPath id="clip0_2036_37">
+																<rect width="19" height="19" fill="white"></rect>
+															</clipPath>
+														</defs>
+													</svg>
+												</div>
+												<img
+													src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/688a2f9dc4c41532e44fa96e_webinar-person.avif"
+													loading="lazy" alt="webinar-person">
+												<div class="font-family-secondary text-xs">Webinar</div>
+											</div>
+											<div class="flex items-center gap-8">
+												<div class="font-family-secondary font-light text-xs">October 1, 2023</div>
+												<div>•</div>
+												<div class="font-family-secondary font-light text-xs">45 minutes</div>
+											</div>
+										</div>
+									</div>
+									<h3 class="font-semibold">Exploring the Future of Technology</h3>
+								</div>
+								<div class="w-full">
+									<p>Join us as we explore the latest trends in technology and their implications for the
+										future.
+									</p>
+								</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
