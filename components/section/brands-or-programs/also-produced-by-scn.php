b4f1060e359cc4bb1b698d6106051662058f1bd2
Ken Vilar <kenvilar@gmail.com>
2025-08-19 10:57:44 +0800

    Integrate components into brands page

--- components/section/brands-or-programs/also-produced-by-scn.php
+++ components/section/brands-or-programs/also-produced-by-scn.php
@@ -8,73 +8,61 @@
 				<div class="mb-20">
 					<h2 class="text-center">Also Produced by SCN</h2>
 				</div>
-				<div class="w-layout-blockcontainer max-w-136 w-full h-1 relative bg-cargogrey/25 w-container">
-					<div class="absolute absolute--r flex items-center pr-32">
-						<div blinking-dot="" class="size-8 rounded-8 bg-primary"></div>
-					</div>
-				</div>
+				<?php
+				get_template_part('components/line-with-blinking-dot', null, [
+					'maxWidthClassnames' => ''
+				]);
+				?>
 			</div>
 			<div class="w-dyn-list">
 				<div role="list" class="grid grid-cols-3 gap-32 md:grid-cols-2 sm:grid-cols-1 w-dyn-items">
-					<div role="listitem" class="w-dyn-item">
-						<a href="/program/supply-chain-is-boring"
-						   class="w-full overflow-hidden rounded-24 hover-shadow2 w-inline-block">
-							<div class="h-396 sm:h-200">
-								<img
-									src="https://cdn.prod.website-files.com/686225582293e0967d2b9375/6889b6644ec390e320f7b9c5_program-supply%20chain%20is%20boring.avif"
-									loading="lazy" alt="Supply Chain is Boring" class="image">
-							</div>
-						</a>
-					</div>
-					<div role="listitem" class="w-dyn-item">
-						<a href="/program/digital-transformers"
-						   class="w-full overflow-hidden rounded-24 hover-shadow2 w-inline-block">
-							<div class="h-396 sm:h-200">
-								<img
-									src="https://cdn.prod.website-files.com/686225582293e0967d2b9375/6889b6421670245c8994f27c_program-digital%20transformers.avif"
-									loading="lazy" alt="Digital Transformers" class="image">
-							</div>
-						</a>
-					</div>
-					<div role="listitem" class="w-dyn-item">
-						<a href="/program/veteran-voices"
-						   class="w-full overflow-hidden rounded-24 hover-shadow2 w-inline-block">
-							<div class="h-396 sm:h-200">
-								<img
-									src="https://cdn.prod.website-files.com/686225582293e0967d2b9375/6889b65154ff2052b5cb113a_program-veteran%20voices.avif"
-									loading="lazy" alt="Veteran Voices" class="image">
-							</div>
-						</a>
-					</div>
-					<div role="listitem" class="w-dyn-item">
-						<a href="/program/business-history"
-						   class="w-full overflow-hidden rounded-24 hover-shadow2 w-inline-block">
-							<div class="h-396 sm:h-200">
-								<img
-									src="https://cdn.prod.website-files.com/686225582293e0967d2b9375/6889b69bf1c472ef92bf40d6_program-this%20week%20in%20business%20history.avif"
-									loading="lazy" alt="This Week In Business History" class="image">
-							</div>
-						</a>
-					</div>
-					<div role="listitem" class="w-dyn-item">
-						<a href="/program/tektok" class="w-full overflow-hidden rounded-24 hover-shadow2 w-inline-block">
-							<div class="h-396 sm:h-200">
-								<img
-									src="https://cdn.prod.website-files.com/686225582293e0967d2b9375/6889b66ff8c941a51e2ad48d_program-tektok.avif"
-									loading="lazy" alt="TEKTOK" class="image">
-							</div>
-						</a>
-					</div>
-					<div role="listitem" class="w-dyn-item">
-						<a href="/program/techquila-sunrise"
-						   class="w-full overflow-hidden rounded-24 hover-shadow2 w-inline-block">
-							<div class="h-396 sm:h-200">
-								<img
-									src="https://cdn.prod.website-files.com/686225582293e0967d2b9375/6889b68b281dc4e7d48bf076_program-techquila%20sunrise.avif"
-									loading="lazy" alt="TECHquila Sunrise" class="image">
-							</div>
-						</a>
-					</div>
+					<?php
+					$defaults_args = [
+						"post_type" => "program",
+						"post_status" => "publish",
+						"posts_per_page" => 6,
+						"offset" => 0,
+						'post_name__in' => [
+							'supply-chain-is-boring',
+							'digital-transformers',
+							'veteran-voices',
+							'business-history',
+							'tektok',
+							'techquila-sunrise'
+						],
+						"orderby" => ["post_name__in" => 'ASC'],
+					];
+					$q = new WP_Query($defaults_args);
+
+					if ($q->have_posts()): ?>
+						<?php
+						while ($q->have_posts()):
+							$q->the_post();
+							?>
+							<a href="<?php
+							the_permalink($q->ID); ?>"
+							   class="w-full overflow-hidden rounded-24 hover-shadow2 w-inline-block">
+								<div class="h-396 sm:h-200">
+									<img
+										src="<?php
+										if (get_field('program_thumbnail_image_upload', $q->ID)) {
+											echo get_field('program_thumbnail_image_upload', $q->ID);
+										} else {
+											echo get_the_post_thumbnail_url($q->ID);
+										}
+										?>"
+										loading="lazy" alt="Supply Chain is Boring" class="image">
+								</div>
+							</a>
+						<?php
+						endwhile;
+						wp_reset_postdata();
+						?>
+					<?php
+					else:
+						echo '<p class="w-full text-center">No items found.</p>';
+					endif;
+					?>
 				</div>
 			</div>
 		</div>
--- components/section/brands-or-programs/featured-upcoming-programming.php
+++ components/section/brands-or-programs/featured-upcoming-programming.php
@@ -8,55 +8,58 @@
 				<div class="mb-20">
 					<h2 class="text-center">Featured Upcoming Programming</h2>
 				</div>
-				<div class="w-layout-blockcontainer max-w-136 w-full h-1 relative bg-cargogrey/25 w-container">
-					<div class="absolute absolute--r flex items-center pr-32">
-						<div blinking-dot="" class="size-8 rounded-8 bg-primary"></div>
-					</div>
-				</div>
+				<?php
+				get_template_part('components/line-with-blinking-dot', null, [
+					'maxWidthClassnames' => ''
+				]);
+				?>
 			</div>
 			<div class="w-layout-blockcontainer max-w-1036 w-container">
 				<div class="w-dyn-list">
 					<div role="list" class="grid grid-cols-2 gap-36 sm:grid-cols-1 w-dyn-items">
-						<div role="listitem" class="w-dyn-item">
-							<a href="/program/supply-chain-now"
-							   class="w-full overflow-hidden rounded-24 hover-shadow2 w-inline-block">
-								<div class="h-496 sm:h-200">
-									<img
-										src="https://cdn.prod.website-files.com/686225582293e0967d2b9375/6889b5ec1e9b6ef3093ff5f8_program-supply%20chain%20now.avif"
-										loading="lazy" alt="" class="image">
-								</div>
-							</a>
-						</div>
-						<div role="listitem" class="w-dyn-item">
-							<a href="/program/supply-chain-now-en-espanol"
-							   class="w-full overflow-hidden rounded-24 hover-shadow2 w-inline-block">
-								<div class="h-496 sm:h-200">
-									<img
-										src="https://cdn.prod.website-files.com/686225582293e0967d2b9375/6889b632c09887a63c454c84_program-supply%20chain%20now%20en%20espanol.avif"
-										loading="lazy" alt="Supply Chain Now en Espanol" class="image">
-								</div>
-							</a>
-						</div>
-						<div role="listitem" class="w-dyn-item">
-							<a href="/program/logistics-with-purpose"
-							   class="w-full overflow-hidden rounded-24 hover-shadow2 w-inline-block">
-								<div class="h-496 sm:h-200">
-									<img
-										src="https://cdn.prod.website-files.com/686225582293e0967d2b9375/6889b626c39d50d03b7206af_program-logistics%20with%20purpose.avif"
-										loading="lazy" alt="Logistics with Purpose®" class="image">
-								</div>
-							</a>
-						</div>
-						<div role="listitem" class="w-dyn-item">
-							<a href="/program/tango-tango"
-							   class="w-full overflow-hidden rounded-24 hover-shadow2 w-inline-block">
-								<div class="h-496 sm:h-200">
-									<img
-										src="https://cdn.prod.website-files.com/686225582293e0967d2b9375/6889b5fc7c2a9c8f523bfa6e_program-tango%20tango.avif"
-										loading="lazy" alt="" class="image">
-								</div>
-							</a>
-						</div>
+						<?php
+						$defaults_args = [
+							"post_type" => "program",
+							"post_status" => "publish",
+							"posts_per_page" => 4,
+							"offset" => 0,
+							'post_name__in' => [
+								'supply-chain-now',
+								'supply-chain-now-en-espanol',
+								'logistics-with-purpose',
+								'tango-tango'
+							],
+							"orderby" => ["post_name__in" => "ASC"],
+						];
+						$q = new WP_Query($defaults_args);
+						if ($q->have_posts()): ?>
+							<?php
+							while ($q->have_posts()):
+								$q->the_post();
+								?>
+								<a href="<?php
+								the_permalink($q->ID); ?>" class="w-full overflow-hidden rounded-24 hover-shadow2 w-inline-block">
+									<div class="h-496 sm:h-200">
+										<img
+											src="<?php
+											if (get_field('program_thumbnail_image_upload', $q->ID)) {
+												echo get_field('program_thumbnail_image_upload', $q->ID);
+											} else {
+												echo get_the_post_thumbnail_url($q->ID);
+											}
+											?>"
+											loading="lazy" alt="" class="image">
+									</div>
+								</a>
+							<?php
+							endwhile;
+							wp_reset_postdata();
+							?>
+						<?php
+						else:
+							echo '<p class="w-full text-center">No items found.</p>';
+						endif;
+						?>
 					</div>
 				</div>
 			</div>
