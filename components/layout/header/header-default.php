b57ebc09723448ffcce6a304d74fd86aae2525b3
Ken Vilar <kenvilar@gmail.com>
2025-08-20 20:08:50 +0800

    Update header-default

--- components/layout/header/header-default.php
+++ components/layout/header/header-default.php
@@ -1,7 +1,492 @@
 <?php
 
 ?>
-<nav>
-	<div class="navmenu">
+<div class="nav">
+	<div data-animation="default" class="navbar w-nav" data-easing2="ease" data-easing="ease" data-collapse="all"
+	     role="banner" data-no-scroll="1" data-duration="400" data-doc-height="1">
+		<div class="nav-padding">
+			<div class="nav-container w-container">
+				<div class="nav-wrapper">
+					<a href="/" aria-current="page" class="nav__brandlink w-nav-brand w--current" aria-label="home">
+						<div>
+							<img
+								src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/68689411745c184d8b7f1835_header-logo.svg"
+								loading="lazy" alt="header logo" class="image h-auto">
+						</div>
+					</a>
+					<nav role="navigation" class="navmenu w-nav-menu">
+						<div class="navlink__group">
+							<div data-delay="0" data-hover="true" class="nav__dropdown w-dropdown" style="max-width: 1250px;">
+								<div class="nav-dropdown__toggle group w-dropdown-toggle" id="w-dropdown-toggle-0"
+								     aria-controls="w-dropdown-list-0" aria-haspopup="menu" aria-expanded="false" role="button"
+								     tabindex="0">
+									<div class="absolute absolute--t flex justify-center opacity-0 group-hover:opacity-100">
+										<div blinking-dot="" class="size-8 rounded-8 bg-primary"></div>
+									</div>
+									<div>About Us</div>
+								</div>
+								<nav class="nav-dropdown__list w-dropdown-list" id="w-dropdown-list-0"
+								     aria-labelledby="w-dropdown-toggle-0">
+									<div class="nav-dropdown__listmenu">
+										<a href="/about" class="nav_dropdown_link w-dropdown-link" tabindex="0">Our Story</a>
+										<a href="/our-team-and-hosts" class="nav_dropdown_link w-dropdown-link" tabindex="0">Our Team &amp;
+											Hosts
+										</a>
+									</div>
+								</nav>
+							</div>
+							<div data-delay="0" data-hover="true" class="nav__dropdown w-dropdown" style="max-width: 1250px;">
+								<div class="nav-dropdown__toggle group w-dropdown-toggle" id="w-dropdown-toggle-1"
+								     aria-controls="w-dropdown-list-1" aria-haspopup="menu" aria-expanded="false" role="button"
+								     tabindex="0">
+									<div class="absolute absolute--t flex justify-center opacity-0 group-hover:opacity-100">
+										<div blinking-dot="" class="size-8 rounded-8 bg-primary"></div>
+									</div>
+									<div>Watch and Listen</div>
+								</div>
+								<nav class="nav-dropdown__list w-dropdown-list" id="w-dropdown-list-1"
+								     aria-labelledby="w-dropdown-toggle-1">
+									<div class="nav-dropdown__listmenu">
+										<a href="/upcoming-live-programming" class="nav_dropdown_link w-dropdown-link" tabindex="0">Upcoming
+											Live Programming
+										</a>
+										<a href="/on-demand-programming" class="nav_dropdown_link w-dropdown-link" tabindex="0">On-Demand
+											Programming
+										</a>
+										<a href="/programs" class="nav_dropdown_link w-dropdown-link" tabindex="0">Brands</a>
+										<ul role="list" class="nav_dropdown_link-sub-list w-list-unstyled">
+											<li>
+												<a href="/program/supply-chain-now" class="nav_dropdown_link-sub" tabindex="0">Supply Chain
+													Now
+												</a>
+											</li>
+											<li>
+												<a href="/program/supply-chain-now-en-espanol" class="nav_dropdown_link-sub" tabindex="0">Supply
+													Chain Now en Español
+												</a>
+											</li>
+											<li>
+												<a href="/program/logistics-with-purpose" class="nav_dropdown_link-sub" tabindex="0">Logistics
+													With Purpose
+												</a>
+											</li>
+											<li>
+												<a href="/program/tango-tango" class="nav_dropdown_link-sub" tabindex="0">Tango Tango</a>
+											</li>
+											<li>
+												<a href="/program/supply-chain-is-boring" class="nav_dropdown_link-sub" tabindex="0">Supply
+													Chain is Boring
+												</a>
+											</li>
+											<li>
+												<a href="/program/digital-transformers" class="nav_dropdown_link-sub" tabindex="0">Digital
+													Transformers
+												</a>
+											</li>
+											<li>
+												<a href="/program/veteran-voices" class="nav_dropdown_link-sub" tabindex="0">Veteran Voices</a>
+											</li>
+											<li>
+												<a href="/program/business-history" class="nav_dropdown_link-sub" tabindex="0">The Week in
+													Business History
+												</a>
+											</li>
+											<li>
+												<a href="/program/tektok" class="nav_dropdown_link-sub" tabindex="0">TEK TOK</a>
+											</li>
+											<li>
+												<a href="/program/techquila-sunrise" class="nav_dropdown_link-sub" tabindex="0">TECHquila
+													Sunrise
+												</a>
+											</li>
+										</ul>
+										<a href="/national-supply-chain-day" class="nav_dropdown_link w-dropdown-link" tabindex="0">National
+											Supply Chain Day
+										</a>
+										<a href="/on-the-road" class="nav_dropdown_link w-dropdown-link" tabindex="0">On The Road</a>
+									</div>
+								</nav>
+							</div>
+							<div data-delay="0" data-hover="true" class="nav__dropdown w-dropdown" style="max-width: 1250px;">
+								<div class="nav-dropdown__toggle group w-dropdown-toggle" id="w-dropdown-toggle-2"
+								     aria-controls="w-dropdown-list-2" aria-haspopup="menu" aria-expanded="false" role="button"
+								     tabindex="0">
+									<div class="absolute absolute--t flex justify-center opacity-0 group-hover:opacity-100">
+										<div blinking-dot="" class="size-8 rounded-8 bg-primary"></div>
+									</div>
+									<div>Work With Us</div>
+								</div>
+								<nav class="nav-dropdown__list w-dropdown-list" id="w-dropdown-list-2"
+								     aria-labelledby="w-dropdown-toggle-2">
+									<div class="nav-dropdown__listmenu">
+										<a href="/work-with-us" class="nav_dropdown_link w-dropdown-link" tabindex="0">Work With Us</a>
+										<a href="/case-studies-customer-stories" class="nav_dropdown_link w-dropdown-link" tabindex="0">Case
+											Studies &amp; Customer Stories
+										</a>
+										<a href="/media-kit" class="nav_dropdown_link w-dropdown-link" tabindex="0">Media Kit</a>
+									</div>
+								</nav>
+							</div>
+							<div data-delay="0" data-hover="true" class="nav__dropdown w-dropdown" style="max-width: 1250px;">
+								<div class="nav-dropdown__toggle group w-dropdown-toggle" id="w-dropdown-toggle-3"
+								     aria-controls="w-dropdown-list-3" aria-haspopup="menu" aria-expanded="false" role="button"
+								     tabindex="0">
+									<div class="absolute absolute--t flex justify-center opacity-0 group-hover:opacity-100">
+										<div blinking-dot="" class="size-8 rounded-8 bg-primary"></div>
+									</div>
+									<div>NOW Campaign Directory</div>
+								</div>
+								<nav class="nav-dropdown__list w-dropdown-list" id="w-dropdown-list-3"
+								     aria-labelledby="w-dropdown-toggle-3">
+									<div class="nav-dropdown__listmenu">
+										<a href="/campaign-directory" class="nav_dropdown_link w-dropdown-link" tabindex="0">Campaign
+											Directory
+										</a>
+										<a href="#" class="nav_dropdown_link pe-none w-dropdown-link" tabindex="0">Network Partners</a>
+										<ul role="list" class="nav_dropdown_link-sub-list w-list-unstyled">
+											<li>
+												<a href="/easypost" class="nav_dropdown_link-sub" tabindex="0">EasyPost</a>
+											</li>
+											<li>
+												<a href="/enable" class="nav_dropdown_link-sub" tabindex="0">Enable</a>
+											</li>
+											<li>
+												<a href="/us-bank" class="nav_dropdown_link-sub" tabindex="0">U.S. Bank</a>
+											</li>
+										</ul>
+										<a href="#" class="nav_dropdown_link pe-none w-dropdown-link" tabindex="0">Campaign Partners</a>
+										<ul role="list" class="nav_dropdown_link-sub-list w-list-unstyled">
+											<li>
+												<a href="/altium" class="nav_dropdown_link-sub" tabindex="0">Altium</a>
+											</li>
+											<li>
+												<a href="/autoscheduler" class="nav_dropdown_link-sub" tabindex="0">AutoScheduler.AI</a>
+											</li>
+											<li>
+												<a href="/bem" class="nav_dropdown_link-sub" tabindex="0">bem</a>
+											</li>
+											<li>
+												<a href="/coupa" class="nav_dropdown_link-sub" tabindex="0">Coupa</a>
+											</li>
+											<li>
+												<a href="/doss" class="nav_dropdown_link-sub" tabindex="0">Doss</a>
+											</li>
+											<li>
+												<a href="/ratelinx" class="nav_dropdown_link-sub" tabindex="0">RateLinx</a>
+											</li>
+											<li>
+												<a href="/sap" class="nav_dropdown_link-sub" tabindex="0">SAP</a>
+											</li>
+											<li>
+												<a href="/trucker-tools" class="nav_dropdown_link-sub" tabindex="0">Trucker Tools</a>
+											</li>
+											<li>
+												<a href="/bastian-solutions" class="nav_dropdown_link-sub" tabindex="0">Bastian Solutions</a>
+											</li>
+											<li>
+												<a href="/dp-world" class="nav_dropdown_link-sub" tabindex="0">DP World</a>
+											</li>
+											<li>
+												<a href="/omp" class="nav_dropdown_link-sub" tabindex="0">OMP</a>
+											</li>
+											<li>
+												<a href="/zebra-technologies" class="nav_dropdown_link-sub" tabindex="0">Zebra Technologies</a>
+											</li>
+										</ul>
+									</div>
+								</nav>
+							</div>
+							<a href="/resource-hub" class="nav__link group w-inline-block">
+								<div class="absolute absolute--t flex justify-center opacity-0 group-hover:opacity-100">
+									<div blinking-dot="" class="size-8 rounded-8 bg-primary"></div>
+								</div>
+								<div>Resource Hub</div>
+							</a>
+						</div>
+						<div class="navlink_cta__group">
+							<a href="/contact" class="btn secondary w-inline-block">
+								<div>Contact Us</div>
+							</a>
+							<a href="https://account.moxo.com/getstarted/#/freeLogin/EnterYourAccount" target="_blank"
+							   class="btn primary-outline w-inline-block">
+								<div>Client Portal</div>
+							</a>
+						</div>
+					</nav>
+					<div class="navmenu__btn w-nav-button" style="-webkit-user-select: text;" aria-label="menu" role="button"
+					     tabindex="0" aria-controls="w-nav-overlay-0" aria-haspopup="menu" aria-expanded="false">
+						<div class="navmenu__icon">
+							<div class="navmenu__icon-bar top"></div>
+							<div class="navmenu__icon-bar middle"></div>
+							<div class="navmenu__icon-bar bottom"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="w-nav-overlay" data-wf-ignore="" id="w-nav-overlay-0"></div>
 	</div>
-</nav>
+	<div class="display-none">
+		<div class="display-none w-embed">
+			<style>
+				.navbar {
+					background-color: red;
+					background-color: transparent;
+				}
+				.nav-padding,
+				.navmenu {
+					padding: 0 20px;
+				}
+				.nav-padding {
+					padding-top: 54px;
+					padding-bottom: 20px;
+				}
+				.nav-container {
+					max-width: 1250px;
+				}
+				.nav-wrapper {
+					display: flex;
+					justify-content: space-between;
+				}
+				.w-nav[data-collapse='all'] .w-nav-menu {
+					display: flex;
+				}
+				.w-nav .w-nav-button {
+					display: none !important;
+				}
+				.nav__brandlink {
+					display: flex;
+					align-items: center;
+				}
+				.navmenu {
+					align-items: stretch;
+					font-size: 14px;
+					/*color: yellow;*/
+				}
+				.navlink__group {
+					display: flex;
+					align-items: center;
+					gap: 21px;
+				}
+				.nav__link {
+					display: flex;
+					align-items: center;
+					text-align: center;
+					height: 100%;
+					line-height: 100%;
+					background-color: blue;
+					background-color: transparent;
+				}
+				.nav__dropdown {
+					height: 100%;
+					margin: 0;
+				}
+				.nav-dropdown__toggle {
+					display: flex;
+					align-items: center;
+					justify-content: space-between;
+					gap: 0;
+					text-align: center;
+					color: inherit;
+					margin: 0;
+					padding: 0;
+					height: 100%;
+					background-color: blue;
+					background-color: transparent;
+				}
+				.nav-dropdown__toggle:hover *,
+				.nav__link:hover,
+				.nav_dropdown_link:hover,
+				.nav_dropdown_link-sub:hover {
+					color: var(--primary) !important;
+				}
+				.nav-dropdown__toggle.w--open .group-hover\:opacity-100 {
+					opacity: 1;
+				}
+				.nav-dropdown__toggle-icon {
+					display: flex;
+					align-items: center;
+					transition: transform 300ms ease;
+				}
+				.nav-dropdown__toggle.w--open .nav-dropdown__toggle-icon {
+					transform: rotate(180deg);
+				}
+				.nav_dropdown_link {
+					padding: 0;
+					color: inherit;
+				}
+				.navlink_cta__group {
+					display: flex;
+					align-items: center;
+				}
+				.navlink_cta__group a {
+					/*color: inherit !important;*/
+				}
+				/* If nav is fixed */
+				.nav.fixed .nav__brandlink {
+					filter: brightness(0) saturate(100%) invert(100%) sepia(0%) saturate(0%) hue-rotate(300deg) brightness(104%) contrast(101%);
+				}
+				.nav.fixed .nav-dropdown__toggle,
+				.nav.fixed .nav__link {
+					color: white;
+				}
+				/* If nav is not fixed but nav background color is text-color or cargo grey */
+				.nav.bg-textcolor:not(.fixed) .navlink__group .nav_dropdown_link,
+				.nav.bg-textcolor:not(.fixed) .navlink__group .nav_dropdown_link-sub {
+					color: var(--text-color);
+				}
+				.nav.bg-textcolor:not(.fixed) .navlink__group .nav-dropdown__toggle {
+					color: white;
+				}
+				@media screen and (max-width: 991px) {
+					.nav {
+						background-color: white;
+					}
+					.nav .nav__brandlink {
+						filter: none !important;
+					}
+					.navmenu {
+						max-width: 1440px;
+						background-color: red;
+						background-color: var(--white);
+					}
+					[data-nav-menu-open] {
+						display: flex !important;
+						flex-direction: column;
+						height: 100%;
+					}
+					.nav-padding {
+						padding-top: 20px;
+						padding-bottom: 20px;
+					}
+					.w-nav[data-collapse='all'] .w-nav-menu {
+						display: none;
+					}
+					.navlink__group {
+						flex-direction: column;
+						align-items: start;
+					}
+					.nav__link {
+						width: 100%;
+					}
+					.navlink_cta__group {
+						display: flex;
+						align-items: stretch;
+						flex-direction: column;
+						padding-bottom: 32px;
+					}
+					.nav__dropdown {
+						width: 100%;
+					}
+					.nav-dropdown__toggle {
+						height: auto;
+					}
+					[blinking-dot] {
+						display: none;
+					}
+					.nav-dropdown__list.w--open {
+						transform: translate(0%);
+					}
+					.nav-dropdown__listmenu {
+						box-shadow: none;
+						border-radius: 0;
+						border: 1px solid transparent;
+						padding-top: 12px;
+						padding-bottom: 0;
+					}
+					.w-nav .w-nav-button {
+						display: block !important;
+						background-color: violet;
+						background-color: transparent;
+					}
+					.navmenu__icon {
+						display: flex;
+						flex-flow: column;
+						justify-content: space-between;
+						gap: 1px;
+						width: 25px;
+						height: 20px;
+						position: relative;
+						background-color: blue;
+						background-color: transparent;
+					}
+					.navmenu__icon-bar {
+						background-color: var(--text-color);
+						width: 100%;
+						height: 2px;
+						opacity: 1;
+						transform: rotate(0) translateY(0) translateX(0);
+						transition: transform 0.3s ease-in-out,
+						opacity 0.2s ease-in-out;
+					}
+					.navmenu__icon-bar.top,
+					.navmenu__icon-bar.bottom {
+						transform-origin: 0;
+					}
+					.w--open .navmenu__icon-bar.top {
+						transform: rotate(45deg) translateX(0px) translateY(0px);
+					}
+					.w--open .navmenu__icon-bar.middle {
+						opacity: 0;
+					}
+					.w--open .navmenu__icon-bar.bottom {
+						transform: rotate(-45deg) translateX(0px) translateY(0px);
+					}
+					/* If nav is fixed */
+					.nav.fixed {
+						position: relative;
+					}
+					.nav.fixed .nav-dropdown__toggle,
+					.nav.fixed .nav__link {
+						color: inherit;
+					}
+					/* If nav is not fixed but nav background color is text-color or cargo grey */
+					.nav.bg-textcolor:not(.fixed) .navlink__group .nav-dropdown__toggle {
+						color: inherit;
+					}
+				}
+			</style>
+		</div>
+		<div class="display-none w-embed w-script">
+			<script>
+				/* ======Fixed Navbar Scroll Animation====== */
+				document.addEventListener("DOMContentLoaded", (event) => {
+					function navWhileScrollAnimation() {
+						window.addEventListener("scroll", () => {
+							const scrollPos = window.scrollY;
+							const offset = 60;
+							if (scrollPos > offset) {
+								document.querySelector(".nav").classList.add("bg-cargogrey");
+							} else if (scrollPos <= offset) {
+								document.querySelector(".nav").classList.remove("bg-cargogrey");
+							}
+						});
+					}
+
+					navWhileScrollAnimation();
+				});
+			</script>
+		</div>
+		<div class="display-none w-embed w-script">
+			<script>
+				document.addEventListener('DOMContentLoaded', (event) => {
+					function clickNavHamburgerBtn() {
+						let navMenuBtn = document.querySelector('.navmenu__btn');
+						let mainWrapper = document.querySelector('.main-wrapper');
+						let wNavOverlay = document.querySelector('.w-nav-overlay');
+						navMenuBtn.addEventListener('click', function (event) {
+							console.log('hey', event);
+							mainWrapper.classList.toggle('display-none');
+							wNavOverlay.classList.toggle('overflow-visible');
+							wNavOverlay.classList.toggle('h-full');
+						});
+					}
+
+					setTimeout(function () {
+						clickNavHamburgerBtn();
+					}, 500);
+				});
+			</script>
+		</div>
+	</div>
+</div>
