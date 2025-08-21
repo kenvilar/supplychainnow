361ed7fe8e7deb6ccbb94ce43f0ab15bd17a413e
Ken Vilar <kenvilar@gmail.com>
2025-08-21 22:59:13 +0800

    Update brands subheadline

--- components/hero/brands.php
+++ components/hero/brands.php
@@ -3,7 +3,7 @@
 ?>
 <section class="section bg-cargogrey text-white rounded-b-100">
 	<div class="site-padding sm:py-60 pt-200 pb-100 relative z-10">
-		<div class="w-layout-blockcontainer pt-20 w-container text-center max-w-960">
+		<div class="w-layout-blockcontainer pt-20 w-container text-center max-w-1220">
 			<div class="mb-20">
 				<img
 					src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/6889a4f52de91445c3439aa0_play-icon.svg"
@@ -13,8 +13,8 @@
 				<h1>Brands</h1>
 			</div>
 			<div>
-				<p class="font-family-alternate font-medium text-xl tracking-[2.4px] capitalize">Explore the voice of supply
-					chain and find what speaks to you
+				<p class="font-family-alternate font-medium text-xl tracking-[2.4px] capitalize">
+					Explore the #1 Voice of Supply Chain and Find What Speaks to You
 				</p>
 			</div>
 		</div>
