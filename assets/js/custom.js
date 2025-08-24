b38bb9f724063dc40629a5322ccdc189519cc514
Ken Vilar <kenvilar@gmail.com>
2025-08-24 10:54:40 +0800

    Add custom select dropdown in searchbar

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,35 +1,172 @@
 document.addEventListener("DOMContentLoaded", function () {
-	function textLimiter() {
-		var elementsWithTextLimit = document.querySelectorAll('[scn-text-limit]');
-		elementsWithTextLimit.forEach(function (element) {
-			var numberOfLines = parseInt(element.getAttribute('scn-text-limit'));
-			if (!isNaN(numberOfLines)) {
-				element.style.overflow = 'hidden';
-				element.style.textOverflow = 'ellipsis';
-				element.style.display = '-webkit-box';
-				element.style.webkitLineClamp = numberOfLines;
-				element.style.webkitBoxOrient = 'vertical';
-			}
-		});
-	}
+  function textLimiter() {
+    var elementsWithTextLimit = document.querySelectorAll("[scn-text-limit]");
+    elementsWithTextLimit.forEach(function (element) {
+      var numberOfLines = parseInt(element.getAttribute("scn-text-limit"));
+      if (!isNaN(numberOfLines)) {
+        element.style.overflow = "hidden";
+        element.style.textOverflow = "ellipsis";
+        element.style.display = "-webkit-box";
+        element.style.webkitLineClamp = numberOfLines;
+        element.style.webkitBoxOrient = "vertical";
+      }
+    });
+  }
 
-	function removeWPAdminBar() {
-		const wpadminbar = document.querySelector("#wpadminbar");
-		const offset = 50;
+  function removeWPAdminBar() {
+    const wpadminbar = document.querySelector("#wpadminbar");
+    const offset = 50;
 
-		window.addEventListener("scroll", () => {
-			const scrollPos = window.scrollY;
+    window.addEventListener("scroll", () => {
+      const scrollPos = window.scrollY;
 
-			if (scrollPos > offset) {
-				if (!wpadminbar.classList.contains("hidden")) {
-					wpadminbar.style.display = 'none';
-				}
-			} else {
-				wpadminbar.style.display = '';
-			}
-		});
-	}
+      if (scrollPos > offset) {
+        if (!wpadminbar.classList.contains("hidden")) {
+          wpadminbar.style.display = "none";
+        }
+      } else {
+        wpadminbar.style.display = "";
+      }
+    });
+  }
 
-	textLimiter();
-	removeWPAdminBar();
+  function customSelectFunc() {
+    // Initialize required DOM elements
+    const form = document.querySelector(".form");
+    const selectBox = document.querySelectorAll(".select");
+
+    // Check if the form element exist on page
+    if (form !== null) {
+      form.addEventListener("submit", (e) => {
+        e.preventDefault();
+      });
+    }
+
+    // Initialize create custom select-box function
+    const customSelectBox = (select) => {
+      // Get of all select options
+      // Convert them from node-list to array
+      const selectOptions = select.querySelectorAll("option");
+      const optionsArray = Array.prototype.slice.call(selectOptions);
+
+      // Create custom Select element and add class select
+      const customSelect = document.createElement("div");
+      customSelect.classList.add("select");
+      select.insertAdjacentElement("afterend", customSelect);
+
+      // Create element for selected option input
+      const selectedOption = document.createElement("div");
+      selectedOption.classList.add("select-option");
+      selectedOption.innerHTML = `
+		<div>${optionsArray[0].textContent}</div>
+		<div style="display:flex;"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="8" viewBox="0 0 14 8" fill="none">
+	  <path d="M12.4198 0.451988L13.4798 1.51299L7.70277 7.29199C7.6102 7.38514 7.50012 7.45907 7.37887 7.50952C7.25762 7.55997 7.12759 7.58594 6.99627 7.58594C6.86494 7.58594 6.73491 7.55997 6.61366 7.50952C6.49241 7.45907 6.38233 7.38514 6.28977 7.29199L0.509766 1.51299L1.56977 0.452987L6.99477 5.87699L12.4198 0.451988Z" fill="currentColor"/>
+	</svg></div>
+		`;
+      customSelect.appendChild(selectedOption);
+
+      // Create element for select menu
+      // Add Class and append it to custom select
+      const selectWrapper = document.createElement("div");
+      selectWrapper.classList.add("select-wrapper");
+      customSelect.appendChild(selectWrapper);
+      selectedOption.addEventListener("click", () => {
+        toggleSelectBox(selectWrapper);
+      });
+
+      // Create wrapper element for select items
+      // Add class and append it to select-menu element
+      const selectMenu = document.createElement("ul");
+      selectMenu.classList.add("select-menu");
+      selectWrapper.appendChild(selectMenu);
+
+      // Loops all options and create custom option
+      // Append it to the select-munu-inner element
+      optionsArray.forEach((option) => {
+        const optionItem = document.createElement("li");
+        optionItem.classList.add("select-item");
+        optionItem.dataset.value = option.value;
+        optionItem.textContent = option.textContent;
+        selectMenu.appendChild(optionItem);
+
+        optionItem.addEventListener("click", () => {
+          setSelectedOption(optionItem, selectedOption, selectWrapper, select);
+        });
+      });
+
+      // Add selected class to first custom select option
+      // Add click event to close custom select-box if clicked outside
+      // Hide the original select-box
+      selectMenu.querySelector("li").classList.add("selected");
+      document.addEventListener("click", (e) => {
+        isClickedOutside(e, customSelect, selectWrapper);
+      });
+      select.style.display = "none";
+    };
+
+    // Toggle for display and hide select-menu
+    const toggleSelectBox = (selectWrapper) => {
+      if (selectWrapper.offsetParent !== null) {
+        selectWrapper.style.display = "none";
+      } else {
+        selectWrapper.style.display = "block";
+      }
+    };
+
+    // Set the selected option function
+    const setSelectedOption = (
+      optionItem,
+      selectedOption,
+      selectWrapper,
+      select,
+    ) => {
+      // Get value and label from clicked custom option
+      const optionValue = optionItem.dataset.value;
+      const optionLabel = optionItem.textContent;
+
+      // Change the text on selected element
+      // Change the value on Selected field
+      selectedOption.innerHTML = `
+		<div>${optionLabel}</div>
+		<div style="display:flex;"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="8" viewBox="0 0 14 8" fill="none">
+	  <path d="M12.4198 0.451988L13.4798 1.51299L7.70277 7.29199C7.6102 7.38514 7.50012 7.45907 7.37887 7.50952C7.25762 7.55997 7.12759 7.58594 6.99627 7.58594C6.86494 7.58594 6.73491 7.55997 6.61366 7.50952C6.49241 7.45907 6.38233 7.38514 6.28977 7.29199L0.509766 1.51299L1.56977 0.452987L6.99477 5.87699L12.4198 0.451988Z" fill="currentColor"/>
+	</svg></div>
+		`;
+      select.value = optionValue;
+
+      // Close the select menu
+      // Remove selected class from prev selected option
+      // Add selected class to clicked option
+      selectWrapper.style.display = "none";
+      selectWrapper.querySelectorAll("li").forEach((li) => {
+        if (li.classList.contains("is-selected")) {
+          li.classList.remove("is-selected");
+        }
+      });
+      optionItem.classList.add("is-selected");
+    };
+
+    // Check if select-box are exist
+    // Loops and create custom select-box for each select element
+    if (selectBox.length > 0) {
+      selectBox.forEach((select) => {
+        customSelectBox(select);
+      });
+    }
+
+    // Close the select if clicked outside select-menu element
+    const isClickedOutside = (e, customSelect, selectWrapper) => {
+      if (e.target.closest(".select") === null) {
+        if (e.target !== customSelect) {
+          if (selectWrapper.offsetParent !== null) {
+            selectWrapper.style.display = "none";
+          }
+        }
+      }
+    };
+  }
+
+  textLimiter();
+  removeWPAdminBar();
+  customSelectFunc();
 });
--- components/ui/searchbar.php
+++ components/ui/searchbar.php
@@ -1,35 +1,57 @@
 <?php
-$site_padding = $args["site_padding"] ?? ""; 
+$site_padding = $args["site_padding"] ?? "";
+$selected_type = isset($_GET['type']) ? sanitize_text_field(wp_unslash($_GET['type'])) : '';
 ?>
-<section class="section">
+<section class="section overflow-visible!">
 	<div class="site-padding sm:py-60 <?php echo esc_attr($site_padding); ?>">
 		<div class="max-w-615 mx-auto">
-			<div class="flex gap-11 justify-between">
-				<div class="max-w-423 w-full md:max-w-full">
-					<form class="relative overflow-hidden rounded-100 border border-secondary/50 bg-[#EBF6FF]"
-						method="get" action="<?php echo esc_url(home_url(add_query_arg([]))); ?>">
-						<label>
-							<input
-								type="search"
-								name="s"
-								value="<?php echo esc_attr(get_search_query()); ?>"
-								placeholder="Search by episode, topic, name, etc..."
-								class="overflow-hidden rounded-100 w-full h-43 py-14 pl-48 pr-12 text-sm font-light placeholder:text-secondary" />
-						</label>
-						<div class="absolute absolute--l flex items-center justify-center pl-22">
-							<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15" fill="none">
-								<path
-									d="M6.31756 11.6351C9.25436 11.6351 11.6351 9.25436 11.6351 6.31756C11.6351 3.38075 9.25436 1 6.31756 1C3.38075 1 1 3.38075 1 6.31756C1 9.25436 3.38075 11.6351 6.31756 11.6351Z"
-									stroke="#4E88B6" stroke-miterlimit="10" />
-								<path d="M14 13.9997L10.4529 10.4526" stroke="#4E88B6" stroke-miterlimit="10" />
-							</svg>
+			<form class="form"
+				method="get" action="<?php echo esc_url(home_url(add_query_arg([]))); ?>">
+				<div class="flex gap-11 justify-between items-stretch">
+					<div class="max-w-423 w-full md:max-w-full">
+
+						<div class="relative overflow-hidden rounded-100 border border-secondary/50 bg-[#EBF6FF]">
+							<label>
+								<input
+									type="search"
+									name="s"
+									value="<?php echo esc_attr(get_search_query()); ?>"
+									placeholder="Search by episode, topic, name, etc..."
+									class="overflow-hidden rounded-100 w-full h-43 py-14 pl-48 pr-12 text-sm font-light placeholder:text-secondary" />
+							</label>
+							<div class="absolute absolute--l flex items-center justify-center pl-22">
+								<svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15" fill="none">
+									<path
+										d="M6.31756 11.6351C9.25436 11.6351 11.6351 9.25436 11.6351 6.31756C11.6351 3.38075 9.25436 1 6.31756 1C3.38075 1 1 3.38075 1 6.31756C1 9.25436 3.38075 11.6351 6.31756 11.6351Z"
+										stroke="#4E88B6" stroke-miterlimit="10" />
+									<path d="M14 13.9997L10.4529 10.4526" stroke="#4E88B6" stroke-miterlimit="10" />
+								</svg>
+							</div>
 						</div>
-					</form>
+
+
+					</div>
+					<div class="max-w-181 w-full md:max-w-full">
+						<label class="sr-only" for="search-type">Filter type</label>
+						<div class="form-select-control">
+							<select id="search-type" name="type" class="select">
+								<option disabled>Sort by Industries</option>
+								<option value="apple">Manufacturing</option>
+								<option value="watermelon">Transportation</option>
+								<option value="banana">Supply Chain</option>
+								<option value="orange">Retail</option>
+								<option value="grape">IT & Services</option>
+								<option value="strawberry">SaaS</option>
+								<option value="mango">Distribution</option>
+								<option value="pineapple">Warehousing</option>
+								<option value="avocado">Supply Chain Tech</option>
+								<option value="blueberry">Logistics/3PLs</option>
+								<option value="papaya">Shipping</option>
+							</select>
+						</div>
+					</div>
 				</div>
-				<div class="max-w-181 w-full md:max-w-full">
-					ok
-				</div>
-			</div>
+			</form>
 		</div>
 	</div>
 </section>
