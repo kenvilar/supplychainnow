6f1cebecd1e34467a711d18c7aa9f6b89fd4c514
Ken Vilar <kenvilar@gmail.com>
2025-08-26 13:44:14 +0800

    Refactor featured content section to replace `card2` with `card1` and add `card_size` parameter for dynamic styling adjustments

--- components/section/home/featured-content.php
+++ components/section/home/featured-content.php
@@ -2,67 +2,66 @@
 
 ?>
 <section class="section">
-	<div class="site-padding sm:py-60 py-88">
-		<div class="max-w-1252 w-container">
-			<div class="mb-44">
-				<div class="mb-20">
-					<h2 class="text-center">Featured Content</h2>
-				</div>
-				<?php
-				get_template_part('components/line-with-blinking-dot', null, [
-					'maxWidthClassnames' => ''
-				]);
-				?>
-			</div>
-			<div class="mb-48">
-				<div class="flex justify-center gap-28 sm:flex-col">
-					<?php
-					echo get_template_part('components/ui/card1', null, [
-						"post_per_page" => 2,
-						'attributes' => [],
-						'classNames' => '',
-					]);
-					?>
-				</div>
-			</div>
-			<div class="mb-64">
-				<div class="flex justify-center gap-32 sm:flex-col">
-					<?php
-					echo get_template_part('components/ui/card2', null, [
-						'q' => [
-							'posts_per_page' => 3,
-							'offset' => 2,
-						],
-						'attributes' => [],
-						'classNames' => '',
-					]);
-					?>
-				</div>
-			</div>
-			<div class="flex justify-center gap-12 sm:flex-col">
-				<?php
-				echo get_template_part('components/ui/btn', null, [
-					'text' => 'Browse All Podcasts',
-					'link' => '/podcasts-and-livestreams',
-					'style' => 'secondary',
-					'class' => '',
-					/*'attributes' => [
-						'target' => '_blank',
-						'rel'    => 'noopener noreferrer',
-					],*/
-				]);
-				echo get_template_part('components/ui/btn', null, [
-					'text' => 'Browse All Webinars',
-					'link' => '/webinars',
-					'style' => 'secondary-outline',
-					'class' => '',
-					/*'attributes' => [
-						'target' => '_blank',
-						'rel'    => 'noopener noreferrer',
-					],*/
-				]);
-				?>
-			</div>
-		</div>
-	</div>
+  <div class="site-padding sm:py-60 py-88">
+    <div class="max-w-1252 w-container">
+      <div class="mb-44">
+        <div class="mb-20">
+          <h2 class="text-center">Featured Content</h2>
+        </div>
+        <?php
+        get_template_part('components/line-with-blinking-dot', null, [
+          'maxWidthClassnames' => ''
+        ]);
+        ?>
+      </div>
+      <div class="mb-48">
+        <div class="flex justify-center gap-28 sm:flex-col">
+          <?php
+          echo get_template_part('components/ui/card1', null, [
+            "post_per_page" => 2,
+            'attributes' => [],
+            'classNames' => '',
+          ]);
+          ?>
+        </div>
+      </div>
+      <div class="mb-64">
+        <div class="flex justify-center gap-32 sm:flex-col">
+          <?php
+          echo get_template_part('components/ui/card1', null, [
+            "post_per_page" => 3,
+            'offset' => 2,
+            'card_size' => 'small',
+            'attributes' => [],
+            'classNames' => '',
+          ]);
+          ?>
+        </div>
+      </div>
+      <div class="flex justify-center gap-12 sm:flex-col">
+        <?php
+        echo get_template_part('components/ui/btn', null, [
+          'text' => 'Browse All Podcasts',
+          'link' => '/podcasts-and-livestreams',
+          'style' => 'secondary',
+          'class' => '',
+          /*'attributes' => [
+            'target' => '_blank',
+            'rel'    => 'noopener noreferrer',
+          ],*/
+        ]);
+        echo get_template_part('components/ui/btn', null, [
+          'text' => 'Browse All Webinars',
+          'link' => '/webinars',
+          'style' => 'secondary-outline',
+          'class' => '',
+          /*'attributes' => [
+            'target' => '_blank',
+            'rel'    => 'noopener noreferrer',
+          ],*/
+        ]);
+        ?>
+      </div>
+    </div>
+  </div>
 </section>
