67c669b01fc47ab4a90313898bcf3027966ca1a4
Ken Vilar <kenvilar@gmail.com>
2025-08-28 16:42:35 +0800

    Update image file paths to use dynamic directory URIs for consistency across components

--- components/layout/footer/cta-footer-3.php
+++ components/layout/footer/cta-footer-3.php
@@ -2,35 +2,37 @@
 
 ?>
 <section class="section rounded-t-100 overflow-hidden text-white select-none">
-	<div class="site-padding sm:py-60 py-144">
-		<div class="w-layout-blockcontainer max-w-788 text-center w-container">
-			<div class="mb-20">
-				<h2 class="text-45">Book a Free Strategy Session</h2>
-			</div>
-			<div class="mb-28">
-				<p class="font-family-alternate font-semibold text-lg">Ready to get in front of 1M+ Supply Chain
-					Professionals?
-				</p>
-			</div>
-			<div class="mb-52">
-				<div class="w-layout-blockcontainer max-w-568 w-container">
-					<p class="tracking-[1.6px]">Book a call today to learn about our packages that include podcasts,
-						livestreams,
-						webinars, and much more.
-					</p>
-				</div>
-			</div>
-			<div class="flex justify-center">
-				<a href="/contact" class="btn primary w-inline-block">
-					<div>Get Started</div>
-				</a>
-			</div>
-		</div>
-	</div>
-	<div class="absolute absolute--full w-full h-full gradient1 rotate-180 z--2"></div>
-	<div class="absolute absolute--full w-full h-full z--1">
-		<img loading="lazy"
-		     src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/685a0cb23b5e3a0d2e89e825_professional-video-shoot-camera.avif"
-		     alt="Camera capturing a professional video shoot setup" class="image opacity-15">
-	</div>
+  <div class="site-padding sm:py-60 py-144">
+    <div class="w-layout-blockcontainer max-w-788 text-center w-container">
+      <div class="mb-20">
+        <h2 class="text-45">Book a Free Strategy Session</h2>
+      </div>
+      <div class="mb-28">
+        <p class="font-family-alternate font-semibold text-lg">Ready to get in front of 1M+ Supply Chain
+          Professionals?
+        </p>
+      </div>
+      <div class="mb-52">
+        <div class="w-layout-blockcontainer max-w-568 w-container">
+          <p class="tracking-[1.6px]">Book a call today to learn about our packages that include podcasts,
+            livestreams,
+            webinars, and much more.
+          </p>
+        </div>
+      </div>
+      <div class="flex justify-center">
+        <a href="/contact" class="btn primary w-inline-block">
+          <div>Get Started</div>
+        </a>
+      </div>
+    </div>
+  </div>
+  <div class="absolute absolute--full w-full h-full gradient1 rotate-180 z--2"></div>
+  <div class="absolute absolute--full w-full h-full z--1">
+    <img
+      loading="lazy"
+      src="<?php
+      echo get_template_directory_uri() . '/assets/img/footer/professional-video-shoot-camera.avif'; ?>"
+      alt="Camera capturing a professional video shoot setup" class="image opacity-15" />
+  </div>
 </section>
--- components/section/national-supply-chain-day/celebrating-national-supply-chain-day.php
+++ components/section/national-supply-chain-day/celebrating-national-supply-chain-day.php
@@ -2,40 +2,41 @@
 
 ?>
 <section class="section">
-	<div class="site-padding sm:py-60 py-92">
-		<div class="w-layout-blockcontainer max-w-832 w-container">
-			<div class="mb-52">
-				<h2 class="text-center">Celebrating National Supply Chain Day℠</h2>
-			</div>
-			<div class="mb-52">
-				<div class="w-layout-blockcontainer max-w-828 w-container">
-					<div class="overflow-hidden rounded-24">
-						<img
-							src="https://cdn.prod.website-files.com/6858d0b082937600c76df99a/6875170fded5985cf1e11603_celebrating%20national%20supply%20chain%20day.avif"
-							loading="lazy" alt="celebrating national supply chain day"
-							class="w-full h-full max-w-none fit-cover">
-					</div>
-				</div>
-			</div>
-			<div class="mb-52">
-				<div class="w-layout-blockcontainer max-w-676 w-container">
-					<p class="text-center tracking-[1.6px]">Join us as Scott W. Luton and National Supply Chain Day® founder,
-						Mary
-						Kate Love, host the special edition livestream as we commemorate National Supply Chain Day®.
-						<br>
-						<br>
-						Tune in as we welcome special guests, share exciting announcements, celebrate, and shine a spotlight on
-						the industry professionals of Supply Chain: the people who connect the world.
-					</p>
-				</div>
-			</div>
-			<div class="flex justify-center sm:flex-col">
-				<div class="flex">
-					<a href="/contact" class="btn primary w-inline-block">
-						<div>Register for the 2026 Event</div>
-					</a>
-				</div>
-			</div>
-		</div>
-	</div>
+  <div class="site-padding sm:py-60 py-92">
+    <div class="w-layout-blockcontainer max-w-832 w-container">
+      <div class="mb-52">
+        <h2 class="text-center">Celebrating National Supply Chain Day℠</h2>
+      </div>
+      <div class="mb-52">
+        <div class="w-layout-blockcontainer max-w-828 w-container">
+          <div class="overflow-hidden rounded-24">
+            <img
+              src="<?php
+              echo get_template_directory_uri() . '/assets/img/national-supply-chain-day/celebrating-national-supply-chain-day.avif'; ?>"
+              loading="lazy" alt="celebrating national supply chain day"
+              class="w-full h-full max-w-none fit-cover">
+          </div>
+        </div>
+      </div>
+      <div class="mb-52">
+        <div class="w-layout-blockcontainer max-w-676 w-container">
+          <p class="text-center tracking-[1.6px]">Join us as Scott W. Luton and National Supply Chain Day® founder,
+            Mary
+            Kate Love, host the special edition livestream as we commemorate National Supply Chain Day®.
+            <br>
+            <br>
+            Tune in as we welcome special guests, share exciting announcements, celebrate, and shine a spotlight on
+            the industry professionals of Supply Chain: the people who connect the world.
+          </p>
+        </div>
+      </div>
+      <div class="flex justify-center sm:flex-col">
+        <div class="flex">
+          <a href="/contact" class="btn primary w-inline-block">
+            <div>Register for the 2026 Event</div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
