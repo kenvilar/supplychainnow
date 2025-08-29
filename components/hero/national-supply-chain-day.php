fb48740de1938b9a13b746ce42e70d3c3a09d0d1
Ken Vilar <kenvilar@gmail.com>
2025-08-29 10:46:31 +0800

    Update hero image logic for National Supply Chain Day

--- components/hero/national-supply-chain-day.php
+++ components/hero/national-supply-chain-day.php
@@ -24,11 +24,12 @@
   <div class="absolute absolute--full w-full h-full">
     <img
       src="<?php
-      if ( has_post_thumbnail( get_the_ID() ) ) {
+      /*if ( has_post_thumbnail( get_the_ID() ) ) {
         echo get_the_post_thumbnail_url( get_the_ID(), 'full' );
       } else {
         echo get_stylesheet_directory_uri() . '/assets/img/hero-img/hero--national-supply-chain-day.avif';
-      }
+      }*/
+      echo get_stylesheet_directory_uri() . '/assets/img/hero-img/hero--national-supply-chain-day.avif';
       ?>"
       loading="lazy" alt="hero-national-supply-chain-day" class="image opacity-10">
   </div>
