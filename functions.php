c9e39f5e62d6a3b4f0cb49699f6563b069274abd
Ken Vilar <kenvilar@gmail.com>
2025-08-29 08:07:20 +0800

    Add `Meet the Team` custom post type

--- functions.php
+++ functions.php
@@ -340,6 +340,43 @@ function hide_editor() {
 	}
 }
 
+function cptui_register_my_cpts_meet_the_team() {
+	/**
+	 * Post Type: Meet the Team.
+	 */
+	$labels = [
+		"name"          => esc_html__( "Meet the Team", "custom-post-type-ui" ),
+		"singular_name" => esc_html__( "Meet the Team", "custom-post-type-ui" ),
+	];
+	$args = [
+		"label"                 => esc_html__( "Meet the Team", "custom-post-type-ui" ),
+		"labels"                => $labels,
+		"description"           => "",
+		"public"                => true,
+		"publicly_queryable"    => true,
+		"show_ui"               => true,
+		"show_in_rest"          => true,
+		"rest_base"             => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"rest_namespace"        => "wp/v2",
+		"has_archive"           => false,
+		"show_in_menu"          => true,
+		"show_in_nav_menus"     => true,
+		"delete_with_user"      => false,
+		"exclude_from_search"   => false,
+		"capability_type"       => "post",
+		"map_meta_cap"          => true,
+		"hierarchical"          => false,
+		"can_export"            => true,
+		"rewrite"               => [ "slug" => "meet_the_team", "with_front" => true ],
+		"query_var"             => true,
+		"supports"              => [ "title", "editor", "thumbnail", "excerpt", "custom-fields" ],
+		"show_in_graphql"       => false,
+	];
+	register_post_type( "meet_the_team", $args );
+}
+add_action( 'init', 'cptui_register_my_cpts_meet_the_team' );
+
 
 //function cptui_register_my_cpts_program()
 //{
