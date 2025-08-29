baa7cc6f16ef30878bd340840459088c167de784
Ken Vilar <kenvilar@gmail.com>
2025-08-29 22:18:39 +0800

    Update featured webinars and episodes sections to dynamically render cards using ACF data

--- components/section/on-demand-programming/featured-episodes.php
+++ components/section/on-demand-programming/featured-episodes.php
@@ -1,42 +1,75 @@
 <?php
 
+$postId                    = get_the_ID();
+$featured_content_podcasts = get_field( 'featured_content_podcasts', $postId );
 ?>
 <section class="section">
-	<div class="site-padding sm:py-60 pt-60 pb-80">
-		<div class="w-layout-blockcontainer max-w-1372 relative w-container">
-			<div class="mb-44">
-				<div class="mb-20">
-					<h2 class="text-center">Featured Episodes</h2>
-				</div>
-				<?php
-				get_template_part("components/line-with-blinking-dot"); ?>
-			</div>
-			<div class="w-layout-blockcontainer max-w-1252 w-container">
-				<div class="w-dyn-list">
-					<div role="list" class="flex justify-center gap-28 sm:flex-col w-dyn-items">
-						<?php
-						echo get_template_part("components/ui/card1", null, [
-							"q" => [
-								"meta_query" => [
-									[
-										"relation" => "AND",
-										[
-											'key'     => '_wp_page_template',
-											'value'   => ['episode-detail.php', 'livestream-detail.php'],
-											'compare' => 'IN',
-											'type'    => 'CHAR',
-										],
-									],
-								],
-							],
-							"post_per_page" => 2,
-							"attributes" => [],
-							"classNames" => "",
-							"noItemsFound" => "",
-						]); ?>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+  <div class="site-padding sm:py-60 pt-60 pb-80">
+    <div class="w-layout-blockcontainer max-w-1372 relative w-container">
+      <div class="mb-44">
+        <div class="mb-20">
+          <h2 class="text-center">Featured Episodes</h2>
+        </div>
+        <?php
+        get_template_part( "components/line-with-blinking-dot" ); ?>
+      </div>
+      <div class="w-layout-blockcontainer max-w-1252 w-container">
+        <div class="w-dyn-list">
+          <div role="list" class="flex justify-center gap-28 sm:flex-col w-dyn-items">
+            <?php
+            if ( $featured_content_podcasts ) {
+              foreach ( $featured_content_podcasts as $index => $item ) {
+                $post = $item['featured_content_podcasts'];
+                echo get_template_part( 'components/ui/card1', null, [
+                  'q'             => [
+                    'post__in'   => [ $post->ID ],
+                    "meta_query" => [
+                      [
+                        "relation" => "AND",
+                        [
+                          'key'     => '_wp_page_template',
+                          'value'   => [ 'episode-detail.php', 'livestream-detail.php' ],
+                          'compare' => 'IN',
+                          'type'    => 'CHAR',
+                        ],
+                      ],
+                    ],
+                  ],
+                  'q_post'        => [
+                    'post__in' => [ $post->ID ],
+                  ],
+                  "post_per_page" => 1,
+                  'attributes'    => [],
+                  'classNames'    => '',
+                ] );
+                if ( $index == 1 ) {
+                  break;
+                }
+              }
+            }
+            /*echo get_template_part("components/ui/card1", null, [
+              "q" => [
+                "meta_query" => [
+                  [
+                    "relation" => "AND",
+                    [
+                      'key'     => '_wp_page_template',
+                      'value'   => ['episode-detail.php', 'livestream-detail.php'],
+                      'compare' => 'IN',
+                      'type'    => 'CHAR',
+                    ],
+                  ],
+                ],
+              ],
+              "post_per_page" => 2,
+              "attributes" => [],
+              "classNames" => "",
+              "noItemsFound" => "",
+            ]);*/
+            ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </section>
--- components/section/on-demand-programming/featured-webinars.php
+++ components/section/on-demand-programming/featured-webinars.php
@@ -1,5 +1,7 @@
 <?php
 
+$postId                    = get_the_ID();
+$featured_content_webinars = get_field( 'featured_content_webinars', $postId );
 ?>
 <section class="section">
   <div class="site-padding sm:py-60 pb-80">
@@ -9,40 +11,72 @@
           <h2 class="text-center">Featured Webinars</h2>
         </div>
         <?php
-        get_template_part("components/line-with-blinking-dot"); ?>
+        get_template_part( "components/line-with-blinking-dot" ); ?>
       </div>
       <div class="w-layout-blockcontainer max-w-1252 w-container">
         <div class="w-dyn-list">
           <div role="list" class="flex justify-center gap-28 sm:flex-col w-dyn-items">
             <?php
-            echo get_template_part("components/ui/card1", null, [
-              'q' => [
-                "post_type" => "page",
-                "post_status" => "publish",
-                "offset" => 0,
-                'no_found_rows' => true,  // set true if not paginating
+            if ( $featured_content_webinars ) {
+              foreach ( $featured_content_webinars as $index => $item ) {
+                $post = $item['featured_content_webinars'];
+                echo get_template_part( 'components/ui/card1', null, [
+                  'q'             => [
+                    'post__in'   => [ $post->ID ],
+                    "meta_query" => [
+                      [
+                        "relation" => "AND",
+                        [
+                          'key'     => '_wp_page_template',
+                          'value'   => [ 'webinar-detail.php' ],
+                          'compare' => 'IN',
+                          'type'    => 'CHAR',
+                        ],
+                      ],
+                    ],
+                  ],
+                  'q_post'        => [
+                    'post__in' => [ $post->ID ],
+                  ],
+                  "post_per_page" => 1,
+                  'attributes'    => [],
+                  'post_type'     => [ 'page' ],
+                  'classNames'    => '',
+                ] );
+                if ( $index == 1 ) {
+                  break;
+                }
+              }
+            }
+            /*echo get_template_part( "components/ui/card1", null, [
+              'q'             => [
+                "post_type"              => "page",
+                "post_status"            => "publish",
+                "offset"                 => 0,
+                'no_found_rows'          => true,  // set true if not paginating
                 'update_post_meta_cache' => false, // set false if not reading lots of meta
                 'update_post_term_cache' => false,
-                "meta_query" => [
+                "meta_query"             => [
                   [
                     "relation" => "AND",
                     [
-                      'key' => '_wp_page_template',
-                      'value' => ['webinar-detail.php'],
+                      'key'     => '_wp_page_template',
+                      'value'   => [ 'webinar-detail.php' ],
                       'compare' => 'IN',
-                      'type' => 'CHAR',
+                      'type'    => 'CHAR',
                     ],
                   ],
                 ],
-                "orderby" => ["menu_order" => "ASC", "date" => "DESC"],
+                "orderby"                => [ "menu_order" => "ASC", "date" => "DESC" ],
               ],
-              'q_post' => [],
+              'q_post'        => [],
               'post_per_page' => 2,
-              'post_type' => ['page'],
-              "attributes" => [],
-              "classNames" => "",
-              "noItemsFound" => "",
-            ]); ?>
+              'post_type'     => [ 'page' ],
+              "attributes"    => [],
+              "classNames"    => "",
+              "noItemsFound"  => "",
+            ] );*/
+            ?>
           </div>
         </div>
       </div>
