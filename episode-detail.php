f536337a577b6eef934855ef4683cb386ea0fdc4
Ken Vilar <kenvilar@gmail.com>
2025-09-03 16:41:44 +0800

    Update redirect path in episode-detail.php and livestream-detail.php

--- episode-detail.php
+++ episode-detail.php
@@ -230,7 +230,7 @@ $youtube_url = $matches[1] ?? '';
                     'site_padding'  => 'px-0! py-0 pb-0',
                     'taxonomy'      => 'tags',
                     'hide_dropdown' => true,
-                    'redirect_to'   => '/on-demand-programming',
+                    'redirect_to'   => '/podcasts-and-livestreams',
                   ] );
                   ?>
                 </div>
