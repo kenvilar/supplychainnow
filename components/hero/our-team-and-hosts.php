81db607b7ad2aaf19cfc07ed75e625059ae03d37
Ken Vilar <kenvilar@gmail.com>
2025-09-07 18:25:53 +0800

    Refactor hero section to dynamically fetch page title and hero image

--- components/hero/our-team-and-hosts.php
+++ components/hero/our-team-and-hosts.php
@@ -1,5 +1,9 @@
 <?php
 
+$postId     = get_the_ID();
+$page_title = get_field( 'Page_Title', $postId ) ?: 'Our Team & Hosts';
+$hero_image = get_field( 'Hero_Image',
+  $postId ) ?: get_stylesheet_directory_uri() . '/assets/img/hero-img/hero--our-team-and-hosts.avif';
 ?>
 <section class="section bg-cargogrey text-white rounded-b-100">
   <div class="site-padding sm:py-60 pt-200 pb-100 relative z-10">
@@ -11,19 +15,17 @@
           loading="lazy" alt="three persons network ">
       </div>
       <div class="mb-16">
-        <h1>Our Team &amp; Hosts</h1>
+        <h1>
+          <?php
+          echo esc_html( $page_title ); ?>
+        </h1>
       </div>
     </div>
   </div>
   <div class="absolute absolute--full w-full h-full">
     <img
       src="<?php
-      if ( has_post_thumbnail( get_the_ID() ) ) {
-        echo get_the_post_thumbnail_url( get_the_ID(), 'full' );
-      } else {
-        echo get_stylesheet_directory_uri() . '/assets/img/hero-img/hero--our-team-and-hosts.avif';
-      }
-      ?>"
+      echo esc_url( $hero_image ); ?>"
       loading="lazy" alt="hero our team and hosts" class="image opacity-10">
   </div>
 </section>
