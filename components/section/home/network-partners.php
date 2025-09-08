68dbddc094db4608e8954028f7c1b6f86519d97b
Ken Vilar <kenvilar@gmail.com>
2025-09-09 00:44:59 +0800

    Refactor Network Partners section to dynamically fetch button text, links, and card data

--- components/section/home/network-partners.php
+++ components/section/home/network-partners.php
@@ -4,6 +4,8 @@ $postID      = get_the_ID();
 $section     = get_field( 'Network_Partners_Section', $postID );
 $title       = esc_html( ! empty( $section['Title'] ) ? $section['Title'] : 'Network Partners' );
 $description = esc_html( ! empty( $section['Description'] ) ? $section['Description'] : 'Supply Chain Now is powered by partnerships. Our Network Partners are top content creators in the supply chain space - discover their content below.' );
+$buttonText  = esc_html( ! empty( $section['Button_Text'] ) ? $section['Button_Text'] : 'Explore More Partners' );
+$buttonLink  = esc_url( ! empty( $section['Button_Link'] ) ? $section['Button_Link'] : '/campaign-directory' );
 ?>
 <div class="rounded-b-100 overflow-hidden relative">
   <section class="section">
@@ -30,55 +32,81 @@ $description = esc_html( ! empty( $section['Description'] ) ? $section['Descript
         </div>
         <div class="mb-48 sm:mb-20">
           <div class="flex gap-24 justify-between sm:flex-col">
-            <a href="/easypost" class="relative w-full overflow-hidden group w-inline-block">
-              <div
-                class="rounded-12 border border-cargogrey/25 bg-white flex justify-center items-center py-28 px-12 h-full group-hover-shadow1">
-                <img
-                  src="<?php
-                  echo get_stylesheet_directory_uri() . '/assets/img/home/easypost.svg'; ?>"
-                  loading="lazy" alt="easypost logo"
-                  class="grayscale-0 opacity-100 group-hover:grayscale group-hover:opacity-20 transition-all" />
-              </div>
-              <div
-                class="absolute absolute--full w-full h-full flex items-center justify-center translate-y-[100%] group-hover:translate-y-0 transition-all duration-500">
-                <div class="btn primary">Click for more</div>
-              </div>
-            </a>
-            <a href="/us-bank" class="relative w-full overflow-hidden group w-inline-block">
-              <div
-                class="rounded-12 border border-cargogrey/25 bg-white flex justify-center items-center py-28 px-12 h-full group-hover-shadow1">
-                <img
-                  src="<?php
-                  echo get_stylesheet_directory_uri() . '/assets/img/home/us-bank.svg'; ?>"
-                  loading="lazy" alt="us-bank logo"
-                  class="grayscale-0 opacity-100 group-hover:grayscale group-hover:opacity-20 transition-all">
-              </div>
-              <div
-                class="absolute absolute--full w-full h-full flex items-center justify-center translate-y-[100%] group-hover:translate-y-0 transition-all duration-500">
-                <div class="btn primary">Click for more</div>
-              </div>
-            </a>
-            <a href="/enable" class="relative w-full overflow-hidden group w-inline-block">
-              <div
-                class="rounded-12 border border-cargogrey/25 bg-white flex justify-center items-center py-28 px-12 h-full group-hover-shadow1">
-                <img
-                  src="<?php
-                  echo get_stylesheet_directory_uri() . '/assets/img/home/enable.svg'; ?>"
-                  loading="lazy" alt="enable logo"
-                  class="grayscale-0 opacity-100 group-hover:grayscale group-hover:opacity-20 transition-all">
-              </div>
-              <div
-                class="absolute absolute--full w-full h-full flex items-center justify-center translate-y-[100%] group-hover:translate-y-0 transition-all duration-500">
-                <div class="btn primary">Click for more</div>
-              </div>
-            </a>
+            <?php
+            if ( ! empty( $section['Cards'] ) ):
+              foreach ( $section['Cards'] as $index => $item ) :
+                $image = $item['Card_Image'];
+                $link = esc_url( $item['Card_Link'] );
+                ?>
+                <a href="<?= $link; ?>" class="relative w-full overflow-hidden group w-inline-block">
+                  <div
+                    class="rounded-12 border border-cargogrey/25 bg-white flex justify-center items-center py-28 px-12 h-full group-hover-shadow1">
+                    <img
+                      src="<?= $image; ?>"
+                      loading="lazy" alt="easypost logo"
+                      class="grayscale-0 opacity-100 group-hover:grayscale group-hover:opacity-20 transition-all" />
+                  </div>
+                  <div
+                    class="absolute absolute--full w-full h-full flex items-center justify-center translate-y-[100%] group-hover:translate-y-0 transition-all duration-500">
+                    <div class="btn primary">Click for more</div>
+                  </div>
+                </a>
+              <?php
+              endforeach;
+            else:
+              ?>
+              <a href="/easypost" class="relative w-full overflow-hidden group w-inline-block">
+                <div
+                  class="rounded-12 border border-cargogrey/25 bg-white flex justify-center items-center py-28 px-12 h-full group-hover-shadow1">
+                  <img
+                    src="<?php
+                    echo get_stylesheet_directory_uri() . '/assets/img/home/easypost.svg'; ?>"
+                    loading="lazy" alt="easypost logo"
+                    class="grayscale-0 opacity-100 group-hover:grayscale group-hover:opacity-20 transition-all" />
+                </div>
+                <div
+                  class="absolute absolute--full w-full h-full flex items-center justify-center translate-y-[100%] group-hover:translate-y-0 transition-all duration-500">
+                  <div class="btn primary">Click for more</div>
+                </div>
+              </a>
+              <a href="/us-bank" class="relative w-full overflow-hidden group w-inline-block">
+                <div
+                  class="rounded-12 border border-cargogrey/25 bg-white flex justify-center items-center py-28 px-12 h-full group-hover-shadow1">
+                  <img
+                    src="<?php
+                    echo get_stylesheet_directory_uri() . '/assets/img/home/us-bank.svg'; ?>"
+                    loading="lazy" alt="us-bank logo"
+                    class="grayscale-0 opacity-100 group-hover:grayscale group-hover:opacity-20 transition-all">
+                </div>
+                <div
+                  class="absolute absolute--full w-full h-full flex items-center justify-center translate-y-[100%] group-hover:translate-y-0 transition-all duration-500">
+                  <div class="btn primary">Click for more</div>
+                </div>
+              </a>
+              <a href="/enable" class="relative w-full overflow-hidden group w-inline-block">
+                <div
+                  class="rounded-12 border border-cargogrey/25 bg-white flex justify-center items-center py-28 px-12 h-full group-hover-shadow1">
+                  <img
+                    src="<?php
+                    echo get_stylesheet_directory_uri() . '/assets/img/home/enable.svg'; ?>"
+                    loading="lazy" alt="enable logo"
+                    class="grayscale-0 opacity-100 group-hover:grayscale group-hover:opacity-20 transition-all">
+                </div>
+                <div
+                  class="absolute absolute--full w-full h-full flex items-center justify-center translate-y-[100%] group-hover:translate-y-0 transition-all duration-500">
+                  <div class="btn primary">Click for more</div>
+                </div>
+              </a>
+            <?php
+            endif;
+            ?>
           </div>
         </div>
         <div class="flex justify-center">
           <?php
           echo get_template_part( 'components/ui/btn', null, [
-            'text'  => 'Explore More Partners',
-            'link'  => '/campaign-directory',
+            'text'  => $buttonText,
+            'link'  => $buttonLink,
             'style' => 'primary',
             'class' => '',
             /*'attributes' => [
