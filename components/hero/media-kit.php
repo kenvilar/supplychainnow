3e58124bbcf9e8edba54fa524b0d20fb2a76dcc3
Ken Vilar <kenvilar@gmail.com>
2025-09-10 20:55:04 +0800

    Support dynamic embed code and content in Media Kit sections

--- components/hero/media-kit.php
+++ components/hero/media-kit.php
@@ -1,5 +1,9 @@
 <?php
 
+$pageID     = get_the_ID();
+$page_title = esc_html( get_field( 'Page_Title', $pageID ) ?: 'Media Kit' );
+$hero_image = esc_url( get_field( 'Hero_Image',
+  $pageID ) ?: get_stylesheet_directory_uri() . '/assets/img/hero-img/hero--media-kit.avif' );
 ?>
 <section class="section bg-cargogrey text-white rounded-b-100">
   <div class="site-padding sm:py-60 pt-200 pb-100 relative z-10">
@@ -11,19 +15,13 @@
           loading="lazy" alt="open envelope">
       </div>
       <div class="mb-16">
-        <h1>Media Kit</h1>
+        <h1><?= $page_title; ?></h1>
       </div>
     </div>
   </div>
   <div class="absolute absolute--full w-full h-full">
     <img
-      src="<?php
-      if ( has_post_thumbnail( get_the_ID() ) ) {
-        echo get_the_post_thumbnail_url( get_the_ID(), 'full' );
-      } else {
-        echo get_stylesheet_directory_uri() . '/assets/img/hero-img/hero--media-kit.avif';
-      }
-      ?>"
+      src="<?= $hero_image; ?>"
       loading="lazy" alt="hero media kit" class="image opacity-10">
   </div>
 </section>
--- components/section/media-kit/section-1.php
+++ components/section/media-kit/section-1.php
@@ -1,13 +1,23 @@
 <?php
 
+$pageId     = get_the_ID();
+$embed_code = get_field( 'Embed_Code', $pageId ) ?: '';
 ?>
 <section class="section">
   <div class="site-padding sm:py-60 py-84">
     <div class="w-layout-blockcontainer max-w-1252 w-container">
       <div class="overflow-hidden rounded-12 relative">
-        <script src="https://js.hsforms.net/forms/embed/49227407.js" defer></script>
-        <div class="hs-form-frame" data-region="na1" data-form-id="df02de9f-5fd6-47e6-adfc-45fda24d037e"
-             data-portal-id="49227407"></div>
+        <?php
+        if ( ! empty( $embed_code ) ):
+          echo $embed_code;
+        else:
+          ?>
+          <script src="https://js.hsforms.net/forms/embed/49227407.js" defer></script>
+          <div class="hs-form-frame" data-region="na1" data-form-id="df02de9f-5fd6-47e6-adfc-45fda24d037e"
+               data-portal-id="49227407"></div>
+        <?php
+        endif;
+        ?>
       </div>
     </div>
   </div>
