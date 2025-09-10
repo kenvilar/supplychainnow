23cd761aa2b8cde6651b431349f691caffe531e8
Ken Vilar <kenvilar@gmail.com>
2025-09-10 08:52:53 +0800

    Support dynamic content in "Putting a Microphone on Meaningful Conversations" section, including title, subtitle, description and button properties

--- components/section/our-story/putting-a-microphone.php
+++ components/section/our-story/putting-a-microphone.php
@@ -1,5 +1,12 @@
 <?php
 
+$pageID      = get_the_ID();
+$section     = get_field( 'Putting_a_Microphone_on_Meaningful_Conversations_Section', $pageID );
+$title       = esc_html( ! empty( $section['Title'] ) ? $section['Title'] : 'Putting a Microphone on Meaningful Conversations' );
+$sub_title   = esc_html( ! empty( $section['Sub_Title'] ) ? $section['Sub_Title'] : 'Important information should flow freely.' );
+$description = esc_html( ! empty( $section['Description'] ) ? $section['Description'] : 'Important information should flow freely.' );
+$button_text = esc_html( ! empty( $section['Button_Text'] ) ? $section['Button_Text'] : 'Explore Sponsorship' );
+$button_link = esc_url( ! empty( $section['Button_Link'] ) ? $section['Button_Link'] : '/media-kit' );
 ?>
 <section class="section">
   <div class="site-padding sm:py-60 py-68">
@@ -7,41 +14,51 @@
       <div class="mb-32">
         <div class="w-layout-blockcontainer max-w-544 w-container">
           <div class="text-center">
-            <h2>Putting a Microphone on Meaningful Conversations</h2>
+            <h2><?= $title; ?></h2>
           </div>
         </div>
       </div>
       <div class="mb-36">
         <div class="text-center">
-          <div class="font-family-alternate font-semibold text-lg text-secondary"><em>Important information should
-              flow freely.</em></div>
+          <div class="font-family-alternate font-semibold text-lg text-secondary">
+            <em><?= $sub_title; ?></em>
+          </div>
         </div>
       </div>
       <div class="mb-44">
-        <p class="tracking-[1.6px]">We take this principle to heart at Supply Chain Now as we produce informative,
-          timely,
-          and indispensable content that supply chain leaders apply to their own business every day.
-          <br>
-          <br>
-          Ranked in the Top 1% of all global podcasts and home to the #1 Supply Chain Podcast on Apple Podcasts,
-          Supply Chain Now is the trusted media platform where industry leaders turn for insights.
-          <br>
-          <br>
-          We’re here to unite everything you need to know about the latest in global supply chain performance in one
-          place. Whether you prefer to listen in, read on, or speak up, we have a format for you.
-          <br>
-          <br>
-          Discover media that connects you directly to the supply chain leaders most frequently sourced for insights
-          as we spotlight the people, technology, best practices, critical issues, and opportunities impacting
-          global
-          supply chain performance.
-        </p>
+        <div class="tracking-[1.6px]">
+          <?php
+          if ( $section['Description'] ):
+            echo $section['Description'];
+          else:
+            ?>
+            We take this principle to heart at Supply Chain Now as we produce informative,
+            timely,
+            and indispensable content that supply chain leaders apply to their own business every day.
+            <br>
+            <br>
+            Ranked in the Top 1% of all global podcasts and home to the #1 Supply Chain Podcast on Apple Podcasts,
+            Supply Chain Now is the trusted media platform where industry leaders turn for insights.
+            <br>
+            <br>
+            We’re here to unite everything you need to know about the latest in global supply chain performance in one
+            place. Whether you prefer to listen in, read on, or speak up, we have a format for you.
+            <br>
+            <br>
+            Discover media that connects you directly to the supply chain leaders most frequently sourced for insights
+            as we spotlight the people, technology, best practices, critical issues, and opportunities impacting
+            global
+            supply chain performance.
+          <?php
+          endif;
+          ?>
+        </div>
       </div>
       <div class="flex justify-center">
         <?php
         echo get_template_part( 'components/ui/btn', null, [
-          'text'  => 'Explore Sponsorship',
-          'link'  => '/media-kit',
+          'text'  => $button_text,
+          'link'  => $button_link,
           'style' => 'primary',
           'class' => '',
           /*'attributes' => [
