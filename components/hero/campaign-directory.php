2b6d5a8c8af8c9fd94eab453e613a8b5a776bf37
Ken Vilar <kenvilar@gmail.com>
2025-09-10 22:53:10 +0800

    Support dynamic embed code, title, and hero image in Campaign Directory components

--- components/hero/campaign-directory.php
+++ components/hero/campaign-directory.php
@@ -1,5 +1,9 @@
 <?php
 
+$pageID     = get_the_ID();
+$page_title = esc_html( get_field( 'Page_Title', $pageID ) ?: 'Campaign Directory' );
+$hero_image = esc_url( get_field( 'Hero_Image',
+  $pageID ) ?: get_stylesheet_directory_uri() . '/assets/img/hero-img/hero--campaign-directory.avif' );
 ?>
 <section class="section bg-cargogrey text--white rounded-b-100">
   <div class="site-padding sm:py-60 pt-200 pb-100 relative z-10">
@@ -11,19 +15,13 @@
           loading="lazy" alt="notebook-with-one-person">
       </div>
       <div class="mb-16">
-        <h1>Campaign Directory</h1>
+        <h1><?= $page_title; ?></h1>
       </div>
     </div>
   </div>
   <div class="absolute absolute--full w-full h-full">
     <img
-      src="<?php
-      if ( has_post_thumbnail( get_the_ID() ) ) {
-        echo get_the_post_thumbnail_url( get_the_ID(), 'full' );
-      } else {
-        echo get_stylesheet_directory_uri() . '/assets/img/hero-img/hero--campaign-directory.avif';
-      }
-      ?>"
+      src="<?= $hero_image; ?>"
       loading="lazy" alt="hero-campaign-directory" class="image opacity-10">
   </div>
 </section>
