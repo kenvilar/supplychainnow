06828049d7851e0e0d7d40515675b13da368ac40
Ken Vilar <kenvilar@gmail.com>
2025-09-19 13:50:02 +0800

    Reformat code

--- single.php
+++ single.php
@@ -1,28 +1,28 @@
 <?php
 
-extract(bridge_qode_get_blog_single_params()); ?>
+extract( bridge_qode_get_blog_single_params() ); ?>
 <?php
 get_header(); ?>
 <?php
-if (have_posts()): ?>
+if ( have_posts() ): ?>
 	<?php
-	while (have_posts()):
+	while ( have_posts() ):
 		the_post(); ?>
 		<?php
 		//get_template_part('title');
 		?>
 		<?php
-		get_template_part("slider"); ?>
+		get_template_part( "slider" ); ?>
 		<?php
-		if ($single_type == "image-title-post")://this post type is full width
+		if ( $single_type == "image-title-post" )://this post type is full width
 			?>
 			<div class="full_width" <?php
-			if ($background_color != "") {
+			if ( $background_color != "" ) {
 				echo " style='background-color:" . $background_color . "'";
 			} ?>>
 			<?php
 			if (
-				isset($bridge_qode_options["overlapping_content"])
+				isset( $bridge_qode_options["overlapping_content"] )
 				&& $bridge_qode_options["overlapping_content"] == "yes"
 			) { ?>
 				<div class="overlapping_content"><div class="overlapping_content_inner">
@@ -37,12 +37,12 @@ if (have_posts()): ?>
 		// post type
 		else: ?>
 			<div class="container"<?php
-			if ($background_color != "") {
+			if ( $background_color != "" ) {
 				echo " style='background-color:" . $background_color . "'";
 			} ?>>
 			<?php
 			if (
-				isset($bridge_qode_options["overlapping_content"])
+				isset( $bridge_qode_options["overlapping_content"] )
 				&& $bridge_qode_options["overlapping_content"] == "yes"
 			) { ?>
 				<div class="overlapping_content"><div class="overlapping_content_inner">
@@ -57,51 +57,51 @@ if (have_posts()): ?>
 		// post type end
 		?>
 		<?php
-		if ($sidebar == "default" || $sidebar == ""): ?>
+		if ( $sidebar == "default" || $sidebar == "" ): ?>
 			<div <?php
-			bridge_qode_class_attribute(implode(" ", $single_class)); ?>>
+			bridge_qode_class_attribute( implode( " ", $single_class ) ); ?>>
 				<?php
-				get_template_part("templates/" . $single_loop, "loop"); ?>
+				get_template_part( "templates/" . $single_loop, "loop" ); ?>
 				<?php
-				if ($single_grid == "no"): ?>
+				if ( $single_grid == "no" ): ?>
 				<div class="grid_section">
 					<div class="section_inner">
 						<?php
 						endif; ?>
 						<?php
-						if ($blog_hide_comments != "yes") {
+						if ( $blog_hide_comments != "yes" ) {
 							//comments_template('', true);
 						} else {
 							echo "<br/><br/>";
 						} ?>
 						<?php
-						if ($single_grid == "no"): ?>
+						if ( $single_grid == "no" ): ?>
 					</div>
 				</div>
 			<?php
 			endif; ?>
 			</div>
 		<?php
-		elseif ($sidebar == "1" || $sidebar == "2"): ?>
+		elseif ( $sidebar == "1" || $sidebar == "2" ): ?>
 			<?php
-			if ($sidebar == "1"): ?>
+			if ( $sidebar == "1" ): ?>
 				<div class="two_columns_66_33 background_color_sidebar grid2 clearfix">
 				<div class="column1">
 			<?php
-			elseif ($sidebar == "2"): ?>
+			elseif ( $sidebar == "2" ): ?>
 				<div class="two_columns_75_25 background_color_sidebar grid2 clearfix">
 				<div class="column1">
 			<?php
 			endif; ?>
 			<div class="column_inner">
 				<div <?php
-				bridge_qode_class_attribute(implode(" ", $single_class)); ?>>
+				bridge_qode_class_attribute( implode( " ", $single_class ) ); ?>>
 					<?php
-					get_template_part("templates/" . $single_loop, "loop"); ?>
+					get_template_part( "templates/" . $single_loop, "loop" ); ?>
 				</div>
 				<?php
-				if ($blog_hide_comments != "yes") {
-					comments_template("", true);
+				if ( $blog_hide_comments != "yes" ) {
+					comments_template( "", true );
 				} else {
 					echo "<br/><br/>";
 				} ?>
@@ -113,9 +113,9 @@ if (have_posts()): ?>
 			</div>
 			</div>
 		<?php
-		elseif ($sidebar == "3" || $sidebar == "4"): ?>
+		elseif ( $sidebar == "3" || $sidebar == "4" ): ?>
 			<?php
-			if ($sidebar == "3"): ?>
+			if ( $sidebar == "3" ): ?>
 				<div class="two_columns_33_66 background_color_sidebar grid2 clearfix">
 				<div class="column1">
 					<?php
@@ -123,7 +123,7 @@ if (have_posts()): ?>
 				</div>
 				<div class="column2">
 			<?php
-			elseif ($sidebar == "4"): ?>
+			elseif ( $sidebar == "4" ): ?>
 				<div class="two_columns_25_75 background_color_sidebar grid2 clearfix">
 				<div class="column1">
 					<?php
@@ -134,13 +134,13 @@ if (have_posts()): ?>
 			endif; ?>
 			<div class="column_inner">
 				<div <?php
-				bridge_qode_class_attribute(implode(" ", $single_class)); ?>>
+				bridge_qode_class_attribute( implode( " ", $single_class ) ); ?>>
 					<?php
-					get_template_part("templates/" . $single_loop, "loop"); ?>
+					get_template_part( "templates/" . $single_loop, "loop" ); ?>
 				</div>
 				<?php
-				if ($blog_hide_comments != "yes") {
-					comments_template("", true);
+				if ( $blog_hide_comments != "yes" ) {
+					comments_template( "", true );
 				} else {
 					echo "<br/><br/>";
 				} ?>
@@ -153,7 +153,7 @@ if (have_posts()): ?>
 		</div>
 		<?php
 	if (
-		isset($bridge_qode_options["overlapping_content"])
+		isset( $bridge_qode_options["overlapping_content"] )
 		&& $bridge_qode_options["overlapping_content"] == "yes"
 	) { ?>
 		</div></div>
