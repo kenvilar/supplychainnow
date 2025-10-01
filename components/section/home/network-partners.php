c5ef237f9f85e478113bce1b8bfb8bfabe54f090
Ken Vilar <kenvilar@gmail.com>
2025-10-01 16:09:47 +0800

    Replace network partners logic with [home_network_partners_cards] shortcode implementation

--- components/section/home/network-partners.php
+++ components/section/home/network-partners.php
@@ -33,72 +33,9 @@ $buttonLink  = esc_url( ! empty( $section['Button_Link'] ) ? $section['Button_Li
 				<div class="mb-48 sm:mb-20">
 					<div class="flex gap-24 justify-between sm:flex-col">
 						<?php
-						if ( ! empty( $section['Cards'] ) ):
-							foreach ( $section['Cards'] as $index => $item ) :
-								$image = $item['Card_Image'];
-								$link = esc_url( $item['Card_Link'] );
-								?>
-								<a href="<?= $link; ?>" class="relative w-full overflow-hidden group w-inline-block">
-									<div
-										class="rounded-12 border border-cargogrey/25 bg-white flex justify-center items-center py-28 px-12 h-full group-hover-shadow1">
-										<img
-											src="<?= $image; ?>"
-											loading="lazy" alt="easypost logo"
-											class="grayscale-0 opacity-100 group-hover:grayscale group-hover:opacity-20 transition-all"/>
-									</div>
-									<div
-										class="absolute absolute--full w-full h-full flex items-center justify-center translate-y-[100%] group-hover:translate-y-0 transition-all duration-500">
-										<div class="btn primary">Click for more</div>
-									</div>
-								</a>
-							<?php
-							endforeach;
-						else:
-							?>
-							<a href="/easypost" class="relative w-full overflow-hidden group w-inline-block">
-								<div
-									class="rounded-12 border border-cargogrey/25 bg-white flex justify-center items-center py-28 px-12 h-full group-hover-shadow1">
-									<img
-										src="<?php
-										echo get_stylesheet_directory_uri() . '/assets/img/home/easypost.svg'; ?>"
-										loading="lazy" alt="easypost logo"
-										class="grayscale-0 opacity-100 group-hover:grayscale group-hover:opacity-20 transition-all"/>
-								</div>
-								<div
-									class="absolute absolute--full w-full h-full flex items-center justify-center translate-y-[100%] group-hover:translate-y-0 transition-all duration-500">
-									<div class="btn primary">Click for more</div>
-								</div>
-							</a>
-							<a href="/us-bank" class="relative w-full overflow-hidden group w-inline-block">
-								<div
-									class="rounded-12 border border-cargogrey/25 bg-white flex justify-center items-center py-28 px-12 h-full group-hover-shadow1">
-									<img
-										src="<?php
-										echo get_stylesheet_directory_uri() . '/assets/img/home/us-bank.svg'; ?>"
-										loading="lazy" alt="us-bank logo"
-										class="grayscale-0 opacity-100 group-hover:grayscale group-hover:opacity-20 transition-all">
-								</div>
-								<div
-									class="absolute absolute--full w-full h-full flex items-center justify-center translate-y-[100%] group-hover:translate-y-0 transition-all duration-500">
-									<div class="btn primary">Click for more</div>
-								</div>
-							</a>
-							<a href="/enable" class="relative w-full overflow-hidden group w-inline-block">
-								<div
-									class="rounded-12 border border-cargogrey/25 bg-white flex justify-center items-center py-28 px-12 h-full group-hover-shadow1">
-									<img
-										src="<?php
-										echo get_stylesheet_directory_uri() . '/assets/img/home/enable.svg'; ?>"
-										loading="lazy" alt="enable logo"
-										class="grayscale-0 opacity-100 group-hover:grayscale group-hover:opacity-20 transition-all">
-								</div>
-								<div
-									class="absolute absolute--full w-full h-full flex items-center justify-center translate-y-[100%] group-hover:translate-y-0 transition-all duration-500">
-									<div class="btn primary">Click for more</div>
-								</div>
-							</a>
-						<?php
-						endif;
+						if ( shortcode_exists( 'home_network_partners_cards' ) ) {
+							do_shortcode( '[home_network_partners_cards]' );
+						}
 						?>
 					</div>
 				</div>
