0f4e46458cc276a67dd249468a369382caca6e0e
Ken Vilar <kenvilar@gmail.com>
2025-10-12 15:24:36 +0800

    Reformat code

--- 404.php
+++ 404.php
@@ -1,47 +1,65 @@
-<?php $bridge_qode_options = bridge_qode_return_global_options(); ?>
 <?php
-set_query_var('header_args', [
-	'nav_classnames' => 'nav-fixed', // '' || 'nav-fixed'
-]);
+$bridge_qode_options = bridge_qode_return_global_options(); ?>
+<?php
+set_query_var( 'header_args', [
+  'nav_classnames' => 'nav-fixed', // '' || 'nav-fixed'
+] );
 get_header();
 ?>
-<style>
-	.title,
-	.title .title_holder {
-		height: 100% !important;
-	}
-
-	.title .title_holder {
-		padding-top: 300px !important;
-		padding-bottom: 100px !important;
-	}
-
-	.title.title_size_large h1 {
-		font-size: 82px !important;
-		line-height: 100px !important;
-	}
-</style>
-<?php get_template_part('title'); ?>
-<div class="container">
-	<?php if (isset($bridge_qode_options['overlapping_content']) && $bridge_qode_options['overlapping_content'] == 'yes') { ?>
-		<div class="overlapping_content">
-			<div class="overlapping_content_inner">
-			<?php } ?>
-			<div class="container_inner default_template_holder">
-				<div class="page_not_found max-w-1000! mx-auto!">
-					<h2><?php if ($bridge_qode_options['404_subtitle'] != ""): echo esc_html($bridge_qode_options['404_subtitle']);
-						else: ?> <?php esc_html_e('The page you are looking for is not found', 'bridge'); ?> <?php endif; ?></h2>
-					<p class="px-0 mx-0"><?php if ($bridge_qode_options['404_text'] != ""): echo esc_html($bridge_qode_options['404_text']);
-											else: ?> <?php esc_html_e('The page you are looking for does not exist. It may have been moved, or removed altogether. Perhaps you can return back to the site\'s homepage and see if you can find what you are looking for.', 'bridge'); ?> <?php endif; ?></p>
-					<div class="separator  transparent center  " style="margin-top:35px;"></div>
-					<p><a itemprop="url" class="btn primary" href="<?php echo esc_url(home_url('/')); ?>"><?php if ($bridge_qode_options['404_backlabel'] != ""): echo esc_html($bridge_qode_options['404_backlabel']);
-																											else: ?> <?php esc_html_e('Back to homepage', 'bridge'); ?> <?php endif; ?></a></p>
-					<div class="separator  transparent center  " style="margin-top:35px;"></div>
-				</div>
-			</div>
-			<?php if (isset($bridge_qode_options['overlapping_content']) && $bridge_qode_options['overlapping_content'] == 'yes') { ?>
-			</div>
-		</div>
-	<?php } ?>
-</div>
-<?php get_footer(); ?>
+  <style>
+		.title,
+		.title .title_holder {
+			height: 100% !important;
+		}
+		.title .title_holder {
+			padding-top: 300px !important;
+			padding-bottom: 100px !important;
+		}
+		.title.title_size_large h1 {
+			font-size: 82px !important;
+			line-height: 100px !important;
+		}
+  </style>
+<?php
+get_template_part( 'title' ); ?>
+  <div class="container">
+    <?php
+    if ( isset( $bridge_qode_options['overlapping_content'] ) && $bridge_qode_options['overlapping_content'] == 'yes' ) { ?>
+    <div class="overlapping_content">
+      <div class="overlapping_content_inner">
+        <?php
+        } ?>
+        <div class="container_inner default_template_holder">
+          <div class="page_not_found max-w-1000! mx-auto!">
+            <h2><?php
+              if ( $bridge_qode_options['404_subtitle'] != "" ): echo esc_html( $bridge_qode_options['404_subtitle'] );
+              else: ?><?php
+                esc_html_e( 'The page you are looking for is not found', 'bridge' ); ?><?php
+              endif; ?></h2>
+            <p class="px-0 mx-0"><?php
+              if ( $bridge_qode_options['404_text'] != "" ): echo esc_html( $bridge_qode_options['404_text'] );
+              else: ?><?php
+                esc_html_e( 'The page you are looking for does not exist. It may have been moved, or removed altogether. Perhaps you can return back to the site\'s homepage and see if you can find what you are looking for.',
+                  'bridge' ); ?><?php
+              endif; ?></p>
+            <div class="separator  transparent center  " style="margin-top:35px;"></div>
+            <p>
+              <a itemprop="url" class="btn primary" href="<?php
+              echo esc_url( home_url( '/' ) ); ?>"><?php
+                if ( $bridge_qode_options['404_backlabel'] != "" ): echo esc_html( $bridge_qode_options['404_backlabel'] );
+                else: ?><?php
+                  esc_html_e( 'Back to homepage', 'bridge' ); ?><?php
+                endif; ?></a>
+            </p>
+            <div class="separator  transparent center  " style="margin-top:35px;"></div>
+          </div>
+        </div>
+        <?php
+        if ( isset( $bridge_qode_options['overlapping_content'] ) && $bridge_qode_options['overlapping_content'] == 'yes' ) { ?>
+      </div>
+    </div>
+  <?php
+  } ?>
+  </div>
+<?php
+get_footer(); ?>
