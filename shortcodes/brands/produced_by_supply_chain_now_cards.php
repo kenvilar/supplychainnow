d5a6139802c0c372de626f2ad618f1e9f733cca3
Ken Vilar <kenvilar@gmail.com>
2025-10-15 11:44:11 +0800

    Add shortcode for displaying supply chain brands with fallback posts

--- shortcodes/brands/produced_by_supply_chain_now_cards.php
+++ shortcodes/brands/produced_by_supply_chain_now_cards.php
@@ -1,21 +1,21 @@
 <?php
 
-if ( ! function_exists( 'brands_featured_programming_cards_shortcode' ) ) {
-  function brands_featured_programming_cards_shortcode( $atts = [] ) {
+if ( ! function_exists( 'brands_produced_by_supply_chain_now_cards_shortcode' ) ) {
+  function brands_produced_by_supply_chain_now_cards_shortcode( $atts = [] ) {
     $pageID  = get_the_ID();
-    $section = get_field( 'Featured_Programming_Section', $pageID );
-
+    $section = get_field( 'Also_Produced_by_Supply_Chain_Now_Section', $pageID );
     ob_start();
     ?>
-    <div role="list" class="grid grid-cols-2 gap-36 sm:grid-cols-1 w-dyn-items">
+    <div role="list" class="grid grid-cols-3 gap-32 md:grid-cols-2 sm:grid-cols-1 w-dyn-items">
       <?php
-      if ( ! empty( $section['Featured_Programming_Repeater'] ) ):
-        foreach ( $section['Featured_Programming_Repeater'] as $index => $item ):
+      if ( ! empty( $section['Item_List_Repeater'] ) ):
+        foreach ( $section['Item_List_Repeater'] as $index => $item ):
           $item = $item['Item'];
           ?>
           <a href="<?php
-          the_permalink( $item ); ?>" class="w-full overflow-hidden rounded-24 hover-shadow2 w-inline-block">
-            <div class="h-496 sm:h-auto">
+          the_permalink( $item ); ?>"
+             class="w-full overflow-hidden rounded-24 hover-shadow2 w-inline-block">
+            <div class="h-396 sm:h-auto">
               <img
                 src="<?php
                 if ( get_field( 'program_thumbnail_image_upload', $item ) ) {
@@ -24,7 +24,7 @@ if ( ! function_exists( 'brands_featured_programming_cards_shortcode' ) ) {
                   echo get_the_post_thumbnail_url( $item );
                 }
                 ?>"
-                loading="lazy" alt="" class="image">
+                loading="lazy" alt="Supply Chain is Boring" class="image">
             </div>
           </a>
         <?php
@@ -33,18 +33,20 @@ if ( ! function_exists( 'brands_featured_programming_cards_shortcode' ) ) {
         $defaults_args = [
           "post_type"              => "brands",
           "post_status"            => "publish",
-          "posts_per_page"         => 4,
+          "posts_per_page"         => 6,
           "offset"                 => 0,
           'no_found_rows'          => true,  // set true if not paginating
           'update_post_meta_cache' => false, // set false if not reading lots of meta
           'update_post_term_cache' => false,
           'post_name__in'          => [
-            'supply-chain-now',
-            'supply-chain-now-en-espanol',
-            'logistics-with-purpose',
-            'tango-tango'
+            'supply-chain-is-boring',
+            'digital-transformers',
+            'veteran-voices',
+            'business-history',
+            'tektok',
+            'techquila-sunrise'
           ],
-          "orderby"                => [ "post_name__in" => "ASC" ],
+          "orderby"                => [ "post_name__in" => 'ASC' ],
         ];
 
         $q = new WP_Query( $defaults_args );
@@ -54,8 +56,9 @@ if ( ! function_exists( 'brands_featured_programming_cards_shortcode' ) ) {
             $q->the_post();
             ?>
             <a href="<?php
-            the_permalink( $q->ID ); ?>" class="w-full overflow-hidden rounded-24 hover-shadow2 w-inline-block">
-              <div class="h-496 sm:h-200">
+            the_permalink( $q->ID ); ?>"
+               class="w-full overflow-hidden rounded-24 hover-shadow2 w-inline-block">
+              <div class="h-396 sm:h-200">
                 <img
                   src="<?php
                   if ( get_field( 'program_thumbnail_image_upload', $q->ID ) ) {
@@ -64,7 +67,7 @@ if ( ! function_exists( 'brands_featured_programming_cards_shortcode' ) ) {
                     echo get_the_post_thumbnail_url( $q->ID );
                   }
                   ?>"
-                  loading="lazy" alt="" class="image">
+                  loading="lazy" alt="Supply Chain is Boring" class="image">
               </div>
             </a>
           <?php
@@ -84,5 +87,5 @@ if ( ! function_exists( 'brands_featured_programming_cards_shortcode' ) ) {
 }
 
 add_action( 'init', function () {
-  add_shortcode( 'brands_featured_programming_cards', 'brands_featured_programming_cards_shortcode' );
+  add_shortcode( 'brands_produced_by_supply_chain_now_cards', 'brands_produced_by_supply_chain_now_cards_shortcode' );
 } );
