e83476b48dae8381dfeab1fcc852a17e1cfebcfd
Ken Vilar <kenvilar@gmail.com>
2025-10-16 20:19:49 +0800

    Add shortcode for webinar search results and update template usage

--- page/on-demand-programming--webinars.php
+++ page/on-demand-programming--webinars.php
@@ -28,9 +28,10 @@ $pageId = get_the_ID();
 			] );
 			scn_render_if_no_filters( 'components/section/on-demand-programming--webinars/recent-webinars' );
 
-			get_template_part( 'components/ui/search_results', null, [
+			/*get_template_part( 'components/ui/search_results', null, [
 				'media_type' => 'webinars',
-			] );
+			] );*/
+			echo do_shortcode( '[on_demand_webinars_search_results]' );
 
 			get_template_part( 'components/layout/footer/cta-footer-2' );
 			?>
