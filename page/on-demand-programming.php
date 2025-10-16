e8f4ae8f9a551cfe3db862d7e03dadbd3046e9c5
Ken Vilar <kenvilar@gmail.com>
2025-10-16 09:41:16 +0800

    Replace template parts with shortcodes for search functionality on on-demand programming page

--- page/on-demand-programming.php
+++ page/on-demand-programming.php
@@ -17,19 +17,21 @@ $pageId = get_the_ID();
 				'tabNumber' => 1,
 			] );
 
-			get_template_part( 'components/ui/searchbar', null, [
+			/*get_template_part( 'components/ui/searchbar', null, [
 				'site_padding' => 'pt-52 pb-40',
 				'taxonomy'     => 'tags',
-			] );
+			] );*/
+			echo do_shortcode( '[searchbar site_padding="pt-52 pb-40" taxonomy="tags"]' );
 
 			scn_render_if_no_filters( 'components/section/on-demand-programming/featured-episodes' );
 			scn_render_if_no_filters( 'components/section/on-demand-programming/featured-webinars' );
 			scn_render_if_no_filters( 'components/section/on-demand-programming/podcast-episodes' );
 			scn_render_if_no_filters( 'components/section/on-demand-programming/on-demand-webinars' );
 
-			get_template_part( 'components/ui/search_results', null, [
+			/*get_template_part( 'components/ui/search_results', null, [
 				'media_type' => 'all-events',
-			] );
+			] );*/
+			echo do_shortcode( '[on_demand_programming_all_content_search_results]' );
 
 			get_template_part( 'components/layout/footer/cta-footer-2' );
 			?>
